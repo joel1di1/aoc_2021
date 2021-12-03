1e7efc92c19ac8264f386da219b6f9a38cf90551
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-03 22:48:55 +0100

    dry

--- lib/day3.rb
+++ lib/day3.rb
@@ -13,48 +13,40 @@ sums = Array.new(@length, 0)
   end
 end
 
-gamma_rate = sums.map{|b| b > (lines.count.to_f/2) ? 1 : 0}.join.to_i(2)
-epsilon_rate = sums.map{|b| b < (lines.count.to_f/2) ? 1 : 0}.join.to_i(2)
+mid_point = (lines.count.to_f/2)
+gamma_rate = sums.map{|b| b > mid_point ? 1 : 0}.join.to_i(2)
+epsilon_rate = sums.map{|b| b < mid_point ? 1 : 0}.join.to_i(2)
 
-pp "Consumption: #{gamma_rate * epsilon_rate}"
+puts "Consumption: #{gamma_rate * epsilon_rate}"
 
 
-def find_most_common(byte_array, position)
-  byte_array.map { |byte| byte[position]}.reduce(:+) >= (byte_array.length.to_f/2) ? 1 : 0
+def find_common(byte_array, position, order)
+  one_more_present = byte_array.map { |byte| byte[position]}.reduce(:+) >= (byte_array.length.to_f/2)
+  (order == :most && one_more_present) || (order == :least && !one_more_present)
 end
 
-def find_least_common(byte_array, position)
-  byte_array.map { |byte| byte[position]}.reduce(:+) >= (byte_array.length.to_f/2) ? 0 : 1
+
+def filter(byte_array, match_value, position)
+  byte_array.select{|byte| !byte[position].zero? == match_value}
 end
 
-
-def filter(byte_array, value, position)
-  byte_array.select{|byte| byte[position] == value}
-end
-
-def determine_oxygen_generator_rating(byte_array, position)
-  if byte_array.length == 1
-    return byte_array.first.join.to_i(2)
-  end
-
-  most_common = find_most_common(byte_array, position)
-  filtered_byte_array = filter(byte_array, most_common, position)
-  determine_oxygen_generator_rating(filtered_byte_array, position + 1)
-end
-
-def determine_co2_scrubber_rating(byte_array, position)
-  raise if position >= @length
+def determine_rating(byte_array, order, position=0)
   return byte_array.first.join.to_i(2) if byte_array.length == 1
 
-  least_common = find_least_common(byte_array, position)
-  filtered_byte_array = filter(byte_array, least_common, position)
-  determine_co2_scrubber_rating(filtered_byte_array, position + 1)
+  common = find_common(byte_array, position, order)
+  filtered_byte_array = filter(byte_array, common, position)
+  determine_rating(filtered_byte_array, order, position + 1)
 end
 
-oxygen_generator_rating = determine_oxygen_generator_rating(byte_array, 0)
-pp oxygen_generator_rating
+def determine_oxygen_generator_rating(byte_array)
+  determine_rating(byte_array, :most)
+end
 
-co2_scrubber_rating = determine_co2_scrubber_rating(byte_array, 0)
-pp co2_scrubber_rating
+def determine_co2_scrubber_rating(byte_array)
+  determine_rating(byte_array, :least)
+end
 
-pp(oxygen_generator_rating * co2_scrubber_rating)
+oxygen_generator_rating = determine_oxygen_generator_rating(byte_array)
+co2_scrubber_rating = determine_co2_scrubber_rating(byte_array)
+
+puts "life support rating: #{oxygen_generator_rating * co2_scrubber_rating}"
