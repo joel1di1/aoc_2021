655efdcc0fb0ba9e206d56d7ae051a0ad1a03134
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-06 18:59:42 +0100

    small changes

--- 2020/day1.rb
+++ 2020/day1.rb
@@ -9,7 +9,8 @@ numbers.each_with_index do |n1, i|
     n2 = numbers[i]
     break if n2.nil?
     break if n2 + n1 > 2020
-    puts(n1 * n2) if n1 + n2 == 2020
+
+    puts("with 2: #{n1 * n2}") if n1 + n2 == 2020
     i += 1
   end
 end
@@ -20,11 +21,13 @@ numbers.each_with_index do |n1, i|
   loop do
     n2 = numbers[i]
     break if n2.nil?
-    j = i+1
+
+    j = i + 1
     loop do
       n3 = numbers[j]
       break if n3.nil?
       break if n3 + n2 + n1 > 2020
+
       puts(n1 * n2 * n3) if n1 + n2 + n3== 2020
       j += 1
     end
