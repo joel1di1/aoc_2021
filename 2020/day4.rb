1c6d9009a51fb54f3db322dd50e5fc08e9de8ba1
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-07 00:34:36 +0100

    2020d4.2

--- 2020/day4.rb
+++ 2020/day4.rb
@@ -8,14 +8,28 @@ def read_passports(file)
 end
 
 def valid?(passport)
-  return true if passport.size == 8
-  return true if passport.size == 7 && passport['cid'].nil?
+  passport.delete 'cid'
+  return false if passport.size != 7
 
-  false
+  return false if !passport['byr'][/^\d{4}$/] || passport['byr'].to_i < 1920 || passport['byr'].to_i > 2002
+  return false if !passport['iyr'][/^\d{4}$/] || passport['iyr'].to_i < 2010 || passport['iyr'].to_i > 2020
+  return false if !passport['eyr'][/^\d{4}$/] || passport['eyr'].to_i < 2020 || passport['eyr'].to_i > 2030
+  return false if !passport['hgt'][/^\d+(cm|in)$/]
+  return false if passport['hgt'][/^\d+cm$/] && (passport['hgt'].to_i < 150 || passport['hgt'].to_i > 193)
+  return false if passport['hgt'][/^\d+in$/] && (passport['hgt'].to_i < 59 || passport['hgt'].to_i > 76)
+  return false if !passport['hcl'][/^#(\d|[a-f]){6}$/]
+  return false if !%w(amb blu brn gry grn hzl oth).include?(passport['ecl'])
+  return false if !passport['pid'][/^\d{9}$/]
+
+  true
 end
 
 def count_valid(passports)
-  passports.select { |passport| valid?(passport) }.count
+  passports.select do |passport|
+    v = valid?(passport)
+    puts passport unless v
+    v
+  end.count
 end
 
 def process(file)
