214bef061667ce92c62684eea20eba32168fa10e
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-06 20:03:29 +0100

    2020d2.2

--- 2020/day2.rb
+++ 2020/day2.rb
@@ -4,19 +4,27 @@ require 'readline'
 
 lines = File.readlines('day2.txt')
 
-selected_lines = lines.select do |line|
+def extract_infos(line)
   a = line.split
   min, max = a.first.split('-').map(&:to_i)
   char = a[1][0]
   password = a[2]
+  [char, min, max, password]
+end
+
+selected_lines = lines.select do |line|
+  char, min, max, password = extract_infos(line)
 
   tally = password.chars.tally
 
   tally[char] && min <= tally[char] && tally[char] <= max
 end
 
-puts selected_lines
+puts "Part 1: #{selected_lines.count}"
 
-puts lines.count
+selected_lines = lines.select do |line|
+  char, position1, position2, password = extract_infos(line)
+  [position1, position2].map{|p| password[p - 1]}.select { |c| c == char }.count == 1
+end
 
-puts selected_lines.count
+puts "Part 2: #{selected_lines.count}"
