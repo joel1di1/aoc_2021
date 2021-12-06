ea46d9f2f1ae24c7227a585f3901c3ea4d36d013
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-06 18:53:01 +0100

    2020d1.2

--- 2020/day1.rb
+++ 2020/day1.rb
@@ -13,3 +13,23 @@ numbers.each_with_index do |n1, i|
     i += 1
   end
 end
+
+# part 2
+
+numbers.each_with_index do |n1, i|
+  loop do
+    n2 = numbers[i]
+    break if n2.nil?
+    j = i+1
+    loop do
+      n3 = numbers[j]
+      break if n3.nil?
+      break if n3 + n2 + n1 > 2020
+      puts(n1 * n2 * n3) if n1 + n2 + n3== 2020
+      j += 1
+    end
+    i+=1
+  end
+end
+
+
