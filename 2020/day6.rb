20a8e383fe0e6964c6dd0e41709e8a3598ecdedf
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-07 01:55:13 +0100

    start2020d7

--- 2020/day6.rb
+++ 2020/day6.rb
@@ -1,22 +1,21 @@
-
 # frozen_string_literal: true
 
 require 'readline'
 require 'set'
 
-groups = File.read('day6.txt').split("\n\n").map{|g| g.split}
+groups = File.read('day6.txt').split("\n\n").map(&:split)
 
-a =  groups.map do |group|
+a = groups.map do |group|
   q = Set.new
-  group.map{|answers| q.merge answers.chars}
+  group.map { |answers| q.merge answers.chars }
   q.size
 end.sum
 
 puts a
 
-a =  groups.map do |group|
+a = groups.map do |group|
   q = Set.new(group[0].chars)
-  group.map{|answers| q = q.intersection answers.chars}
+  group.map { |answers| q = q.intersection answers.chars }
   q.size
 end.sum
 
