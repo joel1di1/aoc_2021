41d3a0cc8ae98a14a4d7d4a7c47280ca1397fb16
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-08 03:28:54 +0100

    2020d8.2

--- 2020/day8.rb
+++ 2020/day8.rb
@@ -4,24 +4,63 @@ require 'byebug'
 require 'readline'
 require 'set'
 
-accumulator = 0
-commands = File.readlines('day8.txt').map{|line| s = line.split; [s[0], s[1].to_i]}
+COMMANDS = File.readlines('day8.txt').map { |line| s = line.split; [s[0], s[1].to_i] }
 
-already_done = Set.new
-index = 0
+def process
+  processed = Set.new
+  index = 0
+  accumulator = 0
 
-while !already_done.include?(index)
-  already_done << index
-  command = commands[index]
-  case command[0]
-  when 'acc'
-    accumulator += command[1]
-    index += 1
-  when 'jmp'
-    index += command[1]
-  when 'nop'
-    index += 1
+  until processed.include?(index) || index == COMMANDS.count
+    processed << index
+
+    command = COMMANDS[index]
+    case command[0]
+    when 'acc'
+      accumulator += command[1]
+      index += 1
+    when 'jmp'
+      index += command[1]
+    when 'nop'
+      index += 1
+    end
+  end
+  [accumulator, processed]
+end
+
+accumulator, processed = process
+
+puts "part1: #{accumulator}"
+
+def select_previous(end_index)
+  (0..COMMANDS.size-1).select do |index|
+    command = COMMANDS[index]
+    (index + 1 == end_index && ['acc', 'nop'].include?(command[0])) ||
+      (command[0] == 'jmp' && (index + command[1]) == end_index)
   end
 end
 
-pp accumulator
+def op_to_change_change(processed, end_index = COMMANDS.count)
+  processed.select do |index|
+    command = COMMANDS[index]
+    (command[0] == 'nop' && index + command[1] == end_index) ||
+      (index if command[0] == 'jmp' && index + 1 == end_index)
+  end
+end
+
+previous = [COMMANDS.count]
+until previous.empty?
+  prev = previous.shift
+  sols = op_to_change_change(processed, prev)
+  break unless sols.empty?
+
+  previous += select_previous(prev)
+end
+sol = sols.first
+puts "to_change: #{sol}, #{COMMANDS[sol]}"
+
+COMMANDS[sol][0] = COMMANDS[sol][0] == 'jmp' ? 'nop' : 'jmp'
+
+accumulator, processed = process
+
+puts "part2: #{accumulator}"
