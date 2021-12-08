05cafdecbd6ee26aa5ccb58682479eb31326ffb3
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-08 21:55:26 +0100

    2020d9.2

--- 2020/day9.rb
+++ 2020/day9.rb
@@ -16,15 +16,30 @@ end
 def process(file, preamble = 25)
   numbers = File.readlines(file).map(&:to_i)
 
-  (preamble..numbers.count).each do |index|
-    return numbers[index] unless sum_in?(numbers[index], numbers[index - preamble, index])
+  invalid_pos = (preamble..numbers.count).find do |index|
+    !sum_in?(numbers[index], numbers[index - preamble, index])
   end
+  invalid = numbers[invalid_pos]
+
+  puts "invalid: #{invalid}"
+
+  (0..numbers.length - 2).each do |start_index|
+    ((start_index + 1)..numbers.length).each do |end_index|
+      contiguous = numbers[start_index..end_index+1]
+      break if contiguous.sum > invalid
+
+      return contiguous.min + contiguous.max if contiguous.sum == invalid
+    end
+  end
+  'KO'
 end
 
-
 sample_result = process('day9_sample.txt', 5)
-expected = 127
+expected = 62
+
+puts "Sample Result: #{sample_result}"
 
 raise "expected: #{expected}, actual: #{sample_result}" unless sample_result == expected
 
 puts "Result: #{process('day9.txt')}"
+
