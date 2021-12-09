c9cb1ce2528a0f26b9d9a21eb8da51233c46c7f5
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-09 02:08:19 +0100

    2020d12.2

--- 2020/11..20/day12.rb
+++ 2020/11..20/day12.rb
@@ -6,28 +6,34 @@ require 'readline'
 def process(file)
   commands = File.readlines(file).map{|l| l.scan(/(\w)(\d+)/).flatten}
 
-  directions = ['N', 'E', 'S', 'W']
-  head = 1 # facing east
+  waypoint = [10, 1] # EAST, NORTH
 
   north = 0
   east = 0
   commands.each do |command|
+    puts "current pos-- east:#{east}, north:#{north}, waypoint: #{waypoint}"
+    puts "applying command: #{command}"
     num = command[1].to_i
-    case command.first == 'F' ? directions[head] : command.first
+    case command.first
     when 'N'
-      north += num
+      waypoint[1] += num
     when 'S'
-      north -= num
+      waypoint[1] -= num
     when 'E'
-      east += num
+      waypoint[0] += num
     when 'W'
-      east -= num
+      waypoint[0] -= num
     when 'L'
-      head = (head - num / 90) % 4
+      ((num / 90) % 4).times do
+        waypoint = [-waypoint[1], waypoint[0]]
+      end
     when 'R'
-      head = (head + num / 90) % 4
+      ((num / 90) % 4).times do
+        waypoint = [waypoint[1], -waypoint[0]]
+      end
     when 'F'
-      north -= num
+      east += num * waypoint[0]
+      north += num * waypoint[1]
     end
   end
 
