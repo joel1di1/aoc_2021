2ff54a384850fa79cce38230063a06b9e2f42f1d
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-12 21:59:43 +0100

    2020d16.2

--- 2020/11..20/day16.rb
+++ 2020/11..20/day16.rb
@@ -7,6 +7,10 @@ class Ticket
     @numbers = numbers.freeze
   end
 
+  def [](index)
+    @numbers[index]
+  end
+
   def all_invalid?(number, rules)
     rules.all? { |rule| rule.invalid?(number) }
   end
@@ -14,11 +18,14 @@ class Ticket
   def invalid_value(rules)
     invalids = @numbers.select { |number| all_invalid?(number, rules) }
     raise "many invalid values in #{@numbers}" if invalids.size > 1
+
     invalids.first
   end
 end
 
 class Rule
+  attr_reader :field
+
   def initialize(field, ranges)
     @ranges = [*ranges].freeze
     @field = field
@@ -26,6 +33,10 @@ class Rule
 
   def invalid?(number)
     @ranges.all? { |range| !range.include?(number) }
+  end
+
+  def valid?(number)
+    !invalid?(number)
   end
 end
 
@@ -68,8 +79,32 @@ def process(file)
   my_ticket, rules, tickets = inputs(file)
 
   sum = tickets.map { |ticket| ticket.invalid_value(rules) }.compact.sum
-
   puts "#{file}, sum: #{sum}"
+
+  tickets.select! { |ticket| ticket.invalid_value(rules).nil? }
+
+  ordered_rules = []
+  (0..rules.size - 1).each do |i|
+    ordered_rules[i] = rules.select do |rule|
+      tickets.all? { |ticket| rule.valid?(ticket[i]) }
+    end
+  end
+
+  until ordered_rules.all? { |candidates| candidates.size == 1 }
+    uniques = ordered_rules.map { |candidates| candidates.first if candidates.size == 1 }.compact
+    uniques.each do |uniq|
+      to_remove = ordered_rules.select { |candidates| candidates.size > 1 && candidates.include?(uniq) }
+      to_remove.each {|candidates| candidates.delete(uniq) }
+    end
+  end
+
+  mult = 1
+  ordered_rules.map!(&:first).each_with_index do |rule, index|
+    mult *= my_ticket[index] if rule.field.start_with?('departure')
+  end
+
+  puts "mult: #{mult}"
+
 end
 
 process('day16_sample.txt')
