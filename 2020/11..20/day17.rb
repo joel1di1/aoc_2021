bda6399c8f880177ae1a516e9d2fa74f7598bd68
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-12 23:18:49 +0100

    2020d17.1

---
+++ 2020/11..20/day17.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+require_relative '../../fwk'
+
+class Universe
+  def initialize(*cubes)
+    clear
+    cubes.each { |cube| @grid[cube.coords] = cube }
+  end
+
+  def at(coords, active: false, create: false)
+    cube = @grid[coords]
+    return cube if cube
+
+    @grid[coords] = Cube.new(coords, active: active) if create
+  end
+
+  def clear
+    @grid = {}
+  end
+
+  def expand!
+    @grid.values.map { |cube| cube.neighbors(create: true) }
+  end
+
+  def cycle
+    expand!
+    next_active_cubes = @grid.values.map do |cube|
+      active_neighbors = cube.neighbors.select(&:active?).count
+      cube if (cube.active? && active_neighbors in 2..3) || (!cube.active? && active_neighbors == 3)
+    end.compact!
+
+    @grid.values.map(&:deactivate!)
+    next_active_cubes.map(&:activate!)
+  end
+
+  def cubes
+    @grid.values
+  end
+end
+
+UNIVERSE = Universe.new
+
+class Cube
+  attr_reader :coords
+
+  def initialize(coords, active: false)
+    @coords = coords.freeze
+    @active = active
+  end
+
+  def neighbors(create: false)
+    (-1..1).map do |x|
+      (-1..1).map do |y|
+        (-1..1).map do |z|
+          n_coords = { x: @coords[:x] + x, y: @coords[:y] + y, z: @coords[:z] + z }
+          UNIVERSE.at(n_coords, create: create) if n_coords != coords
+        end
+      end
+    end.flatten.compact
+  end
+
+  def ==(other)
+    coords == other.coords
+  end
+
+  def <=>(other)
+    coords.to_s <=> other.coords.to_s
+  end
+
+  def to_s
+    "#{coords.to_s} | #{@active}"
+  end
+
+  def activate!
+    @active = true
+  end
+
+  def active?
+    @active
+  end
+
+  def deactivate!
+    @active = false
+  end
+end
+
+def process(file)
+  UNIVERSE.clear
+  File.readlines(file).map.with_index do |line, x|
+    line.strip.chars.each_with_index do |c, y|
+      UNIVERSE.at({ x: x, y: y, z: 1 }, active: c == '#', create: true)
+    end
+  end
+
+  6.times { UNIVERSE.cycle }
+  puts UNIVERSE.cubes.select(&:active?).count
+end
+
+process('day17_sample.txt')
+process('day17.txt')
