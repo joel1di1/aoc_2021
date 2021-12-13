e36c293b45debc89aa12bbbf0b736d175119bf4a
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-14 00:56:18 +0100

    green autocommit

--- 2020/11..20/day18.rb
+++ 2020/11..20/day18.rb
@@ -4,10 +4,17 @@ require_relative '../../fwk'
 
 
 def evaluate(str)
+  str = str.gsub(/\s/,'')
+  while str[/((\d+)[+*](\d+))[+*]/]
+    expr = Regexp.last_match[1]
+    res = eval(expr)
+    str = str.gsub(/((\d+)[+*](\d+))/, res.to_s)
+  end
   eval(str)
 end
 
 assert_eq 6, evaluate('2 * 3')
 assert_eq 5, evaluate('2 + 3')
+assert_eq 10, evaluate('2 + 3 * 2')
 
 `git add . && git commit -am 'green autocommit'`
