7f37b087da8f1b995c40ed9c4616738f6222d5d8
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-14 00:59:57 +0100

    green autocommit

--- 2020/11..20/day18.rb
+++ 2020/11..20/day18.rb
@@ -5,16 +5,17 @@ require_relative '../../fwk'
 
 def evaluate(str)
   str = str.gsub(/\s/,'')
-  while str[/((\d+)[+*](\d+))[+*]/]
+  while str[/^((\d+)[+*](\d+))[+*]/]
     expr = Regexp.last_match[1]
     res = eval(expr)
-    str = str.gsub(/((\d+)[+*](\d+))/, res.to_s)
+    str = str.gsub(/^((\d+)[+*](\d+))/, res.to_s)
   end
   eval(str)
 end
 
-assert_eq 6, evaluate('2 * 3')
-assert_eq 5, evaluate('2 + 3')
-assert_eq 10, evaluate('2 + 3 * 2')
+# assert_eq 6, evaluate('2 * 3')
+# assert_eq 5, evaluate('2 + 3')
+# assert_eq 10, evaluate('2 + 3 * 2')
+assert_eq 71, evaluate('1 + 2 * 3 + 4 * 5 + 6')
 
 `git add . && git commit -am 'green autocommit'`
