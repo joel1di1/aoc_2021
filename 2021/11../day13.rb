4b7dfba68a9d220fe2b019617dbddce3ef14accb
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-14 00:26:23 +0100

    2021d13.2

--- 2021/11../day13.rb
+++ 2021/11../day13.rb
@@ -84,6 +84,10 @@ class Grid
   end
 end
 
+def sym(number, axe_index)
+  number > axe_index  ? number - 2 * (number - axe_index) : number
+end
+
 def process(file)
   grid = Grid.new
   lines = File.readlines(file).map(&:strip)
@@ -107,12 +111,28 @@ def process(file)
     lines.select! { |line| line && line.size >= 2 }.each(&:sort!)
     dots_to_remove = lines.each_with_object([]) do |dot_list, dots_to_remove|
       dots_to_remove.concat(dot_list.select do |dot|
-        dot[check_on] < fold[:index] && dot_list.any? { |other| other != dot && (fold[:index] - other[check_on]).abs == (fold[:index] - dot[check_on]).abs }
+        dot[check_on] > fold[:index] && dot_list.any? { |other| other != dot && (fold[:index] - other[check_on]).abs == (fold[:index] - dot[check_on]).abs }
       end)
     end
     all_dots -= dots_to_remove
+
+    all_dots.map! do |dot|
+      if fold[:axe] == 'x'
+        [sym(dot[0], fold[:index]), dot[1]]
+      else
+        [dot[0], sym(dot[1], fold[:index])]
+      end
+    end
+    # all_dots.uniq!
+
     puts "after fold #{fold}, count: #{all_dots.count}"
   end
+  x_size = all_dots.map { |d| d[0] }.max
+  y_size = all_dots.map { |d| d[1] }.max
+  grid = Array.new(y_size+1) { Array.new(x_size+1, ' ') }
+
+  all_dots.each { |x, y| grid[y][x] = '#' }
+  puts grid.map{|line| line.join}.join("\n")
 end
 
 # process('day13_sample.txt')
