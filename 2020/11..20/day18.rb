62718505df8aba352b5113183641109fbed375de
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-14 01:34:56 +0100

    green autocommit

--- 2020/11..20/day18.rb
+++ 2020/11..20/day18.rb
@@ -27,6 +27,9 @@ assert_eq 10, evaluate('2 + 3 * 2')
 assert_eq 231, evaluate('1 + 2 * 3 + 4 * 5 + 6')
 assert_eq 46, evaluate('2 * 3 + (4 * 5)')
 assert_eq 1445, evaluate('5 + (8 * 3 + 9 + 3 * 4 * 3)')
+assert_eq 669060, evaluate('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))')
+assert_eq 23340, evaluate('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2')
+
 
 
 puts File.readlines('day18.txt').map{|l| evaluate(l)}.sum
