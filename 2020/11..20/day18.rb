1bb8774d2a743193815cbd37cff5dc54576ae707
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-14 01:33:42 +0100

    green autocommit

--- 2020/11..20/day18.rb
+++ 2020/11..20/day18.rb
@@ -8,6 +8,11 @@ def evaluate(str)
     str = str.gsub(match[0], evaluate(match[1]).to_s)
   end
 
+  while match = str.scan(/(\d+[+]\d+)/)[0]
+    str = str.gsub(match[0], eval(match[0]).to_s)
+  end
+
+
   while str[/^((\d+)[+*](\d+))[+*]/]
     expr = Regexp.last_match[1]
     res = eval(expr)
@@ -19,11 +24,9 @@ end
 assert_eq 6, evaluate('2 * 3')
 assert_eq 5, evaluate('2 + 3')
 assert_eq 10, evaluate('2 + 3 * 2')
-assert_eq 71, evaluate('1 + 2 * 3 + 4 * 5 + 6')
-assert_eq 26, evaluate('2 * 3 + (4 * 5)')
-assert_eq 49, evaluate('3 + (4 * 5) * 2 + 3')
-assert_eq 12240, evaluate('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))')
-assert_eq 13632, evaluate('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2')
+assert_eq 231, evaluate('1 + 2 * 3 + 4 * 5 + 6')
+assert_eq 46, evaluate('2 * 3 + (4 * 5)')
+assert_eq 1445, evaluate('5 + (8 * 3 + 9 + 3 * 4 * 3)')
 
 
 puts File.readlines('day18.txt').map{|l| evaluate(l)}.sum
