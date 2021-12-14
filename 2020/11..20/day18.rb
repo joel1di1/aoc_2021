93f0f4d4b5eae0ea82e52993604662023f617fcd
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-14 01:56:06 +0100

    green autocommit

--- 2020/11..20/day18.rb
+++ 2020/11..20/day18.rb
@@ -12,25 +12,27 @@ def evaluate(str)
     str = str.gsub(match[0], eval(match[0]).to_s)
   end
 
-
-  while str[/^((\d+)[+*](\d+))[+*]/]
-    expr = Regexp.last_match[1]
-    res = eval(expr)
-    str = str.gsub(/^((\d+)[+*](\d+))/, res.to_s)
-  end
   eval(str)
 end
+#
+# assert_eq 6, evaluate('2 * 3')
+# assert_eq 5, evaluate('2 + 3')
+# assert_eq 0, evaluate('0 * 1 + 1')
+# assert_eq 1, evaluate('(0 * 1) + 1')
+# assert_eq 1, evaluate('(0 * 1) + 1 * ((0 * 1) + 1)')
+#
+# assert_eq 465, evaluate('3 * 1 + ((2 * 4 + 3) * (2 * 4) + 3)')
 
-assert_eq 6, evaluate('2 * 3')
-assert_eq 5, evaluate('2 + 3')
+assert_eq 7650, evaluate('1 * 2 * (4 + 5 + 6) * (7 + 8) * (9 + 8) ')
+
+
+
 assert_eq 10, evaluate('2 + 3 * 2')
 assert_eq 231, evaluate('1 + 2 * 3 + 4 * 5 + 6')
 assert_eq 46, evaluate('2 * 3 + (4 * 5)')
 assert_eq 1445, evaluate('5 + (8 * 3 + 9 + 3 * 4 * 3)')
 assert_eq 669060, evaluate('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))')
 assert_eq 23340, evaluate('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2')
-
-
 
 puts File.readlines('day18.txt').map{|l| evaluate(l)}.sum
 
