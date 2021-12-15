9b0fa8216563af52ada0ad5b9c1c6df9187536cf
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-15 04:52:54 +0100

    clean

--- 2021/11../day14.rb
+++ 2021/11../day14.rb
@@ -38,36 +38,6 @@ end
 process("day14_sample.txt", 10)
 process("day14.txt", 10)
 
-def count_seq(sequence, rules, nb_iteration, cache = {})
-  cache_key = [sequence, nb_iteration]
-  cached = cache[cache_key]
-  if cached
-    # puts "hit cache for #{cache_key}"
-    cached
-  else
-    # puts "compute #{cache_key}"
-    new_value = if sequence.size <= 1 || nb_iteration.zero?
-      sequence.chars.tally
-    else
-      sequence = step(sequence, rules)
-      pairs = (0..sequence.size - 2).map { |i| sequence[i, 2] }
-      hashes = pairs.map do |pair|
-        count_seq(pair, rules, nb_iteration -1, cache)
-      end
-      res = hashes.each_with_object({}) do |h, acc|
-        h.each do |char, count|
-          acc[char] ||= 0
-          acc[char] += count
-        end
-      end
-      pairs[0..-2].map { |s| s[-1] }.each { |c| res[c] -= 1 }
-      res
-    end
-    cache[[sequence, nb_iteration]] = new_value
-    new_value
-  end
-end
-
 def part2(file, nb_iter, cache = {})
   rules, sequence = read_inputs(file)
   start = sequence[0]
@@ -86,7 +56,6 @@ def part2(file, nb_iter, cache = {})
     end
   end
 
-  puts pair_occurences
   h = pair_occurences.each_with_object({}) do |entry, h|
     pair, occurence = entry
     pair.chars.each do |c|
@@ -101,21 +70,7 @@ end
 
 rules = { 'CN' => 'C' }
 
-assert_eq({ 'N' => 1 }, count_seq('N', rules, 0))
-assert_eq({ 'N' => 1, 'C' => 1 }, count_seq('CN', rules, 0))
-assert_eq({ 'N' => 2, 'C' => 2 }, count_seq('CNCN', rules, 0))
-
-assert_eq({ 'N' => 1, 'C' => 2 }, count_seq('CN', rules, 1))
-
-# rules, sequence = read_inputs('day14_sample.txt')
-# assert_eq({ 'N' => 2, 'C' => 2, 'B' => 2, 'H' => 1 }, count_seq(sequence, rules, 1))
-
 assert_eq 1588, part2('day14_sample.txt', 10)
 assert_eq 2194, part2("day14.txt", 10)
 
-# cache = {}
-# part2("day14.txt", 10, cache)
 puts part2("day14.txt", 40)
-
-puts 'YOUPI !!!'
-`git add . && git commit -am 'green autocommit'`
