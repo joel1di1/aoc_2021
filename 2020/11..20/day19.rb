e86039843b4d7b405f974c1d5559dfb13f2c54d9
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-16 00:00:39 +0100

    2020d19.2

--- 2020/11..20/day19.rb
+++ 2020/11..20/day19.rb
@@ -7,7 +7,22 @@ class OrderRule
     @list = str.split.map(&:to_i)
   end
 
-  def to_re(rules, str_rules)
+  def to_re(rules, str_rules, _8or11: nil)
+    if _8or11 == 8
+      # 8: 42 | 42 8
+      re_42 = convert_rule(42, rules, str_rules)
+      return "(#{re_42})+"
+    end
+    if _8or11 == 11
+      # 11: 42 31 | 42 11 31
+      re_42 = convert_rule(42, rules, str_rules)
+      re_31 = convert_rule(31, rules, str_rules)
+
+      tmp = (1..50).to_a.map do |i|
+        "(#{re_42}){#{i}}(#{re_31}){#{i}}"
+      end.join(')|(')
+      return "((#{tmp}))"
+    end
     @list.map { |rule_index| convert_rule(rule_index, rules, str_rules) }.join
   end
 end
@@ -18,10 +33,10 @@ class OrRule
     @right_list = str2.split.map(&:to_i)
   end
 
-  def to_re(rules, str_rules)
+  def to_re(rules, str_rules, _8or11: false)
     left = @left_list.map { |rule_index| convert_rule(rule_index, rules, str_rules) }.join
     right = @right_list.map { |rule_index| convert_rule(rule_index, rules, str_rules) }.join
-    "((#{left})|(#{right}))"
+    "(#{left}|#{right})"
   end
 end
 
@@ -30,13 +45,15 @@ class ReRule
     @re = str
   end
 
-  def to_re(_rules, _str_rules)
+  def to_re(_rules, _str_rules, _8or11: false)
     @re
   end
 end
 
 def convert_rule(index, rules, str_rules)
-  str_rules[index] ||= rules[index].to_re(rules, str_rules)
+  return nil unless rules[index]
+
+  str_rules[index] ||= rules[index].to_re(rules, str_rules, _8or11: [8, 11].include?(index) && index)
 end
 
 def readinputs(file)
@@ -71,4 +88,49 @@ str_rules = []
   str_rules[i] ||= convert_rule(i, rules, str_rules)
 end
 
-pp messages.select{|msg| msg[/^#{str_rules[0]}$/]}.count
+all = [
+  'abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa',
+  'bbabbbbaabaabba',
+  'babbbbaabbbbbabbbbbbaabaaabaaa',
+  'aaabbbbbbaaaabaababaabababbabaaabbababababaaa',
+  'bbbbbbbaaaabbbbaaabbabaaa',
+  'bbbababbbbaaaaaaaabbababaaababaabab',
+  'ababaaaaaabaaab',
+  'ababaaaaabbbaba',
+  'baabbaaaabbaaaababbaababb',
+  'abbbbabbbbaaaababbbbbbaaaababb',
+  'aaaaabbaabaaaaababaa',
+  'aaaabbaaaabbaaa',
+  'aaaabbaabbaaaaaaabbbabbbaaabbaabaaa',
+  'babaaabbbaaabaababbaabababaaab',
+  'aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba',
+]
+
+matches = [
+  'bbabbbbaabaabba',
+  'babbbbaabbbbbabbbbbbaabaaabaaa',
+  'aaabbbbbbaaaabaababaabababbabaaabbababababaaa',
+  'bbbbbbbaaaabbbbaaabbabaaa',
+  'bbbababbbbaaaaaaaabbababaaababaabab',
+  'ababaaaaaabaaab',
+  'ababaaaaabbbaba',
+  'baabbaaaabbaaaababbaababb',
+  'abbbbabbbbaaaababbbbbbaaaababb',
+  'aaaaabbaabaaaaababaa',
+  'aaaabbaabbaaaaaaabbbabbbaaabbaabaaa',
+  'aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba',
+]
+
+reg = /^(#{str_rules[0]})$/
+
+begin
+  # matches.each do |msg|
+  #   assert msg[reg], msg: "#{msg} should have match"
+  # end
+  # (all - matches).each do |msg|
+  #   assert !msg[reg], msg: "#{msg} should NOT have match"
+  # end
+  pp messages.select { |msg| msg[reg] }.count
+rescue StandardError => e
+  pp e.message[0..100]
+end
--- fwk.rb
+++ fwk.rb
@@ -4,10 +4,11 @@ require 'readline'
 require 'byebug'
 require 'set'
 
-def assert_eq(expected, actual)
+def assert_eq(expected, actual, msg: nil)
   raise "Expected #{expected} but received #{actual}" if expected != actual
 end
 
-def assert(actual)
-  raise "Expected truthy but received #{actual}" unless actual
+def assert(actual, msg: nil)
+  msg ||= "Expected truthy but received #{actual}"
+  raise msg unless actual
 end
