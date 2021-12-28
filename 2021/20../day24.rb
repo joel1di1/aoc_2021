8cd94e3b03eead87307bda6b16d3b394b85e223b
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2021-12-28 11:10:46 +0100

    2021d24.1

--- 2021/20../day24.rb
+++ 2021/20../day24.rb
@@ -9,10 +9,6 @@ class Alu
     puts "#{msg}: #{[w, x, y, z].join(',')}"
   end
 
-  def inp(_a = nil)
-    @monad_input.shift
-  end
-
   def mul(a, b)
     a * b
   end
@@ -40,20 +36,22 @@ class Alu
   end
 
   def legacy
-    z = block(inp, 0, 1, 13, 5)
-    z = block(inp, z, 1, 15, 14)
-    z = block(inp, z, 1, 15, 15)
-    z = block(inp, z, 1, 11, 16)
-    z = block(inp, z, 26, -16, 8)
-    z = block(inp, z, 26, -11, 9)
-    z = block(inp, z, 26, -6, 2)
-    z = block(inp, z, 1, 11, 13)
-    z = block(inp, z, 1, 10, 16)
-    z = block(inp, z, 26, -10, 6)
-    z = block(inp, z, 26, -8, 6)
-    z = block(inp, z, 26, -11, 9)
-    z = block(inp, z, 1, 12, 11)
-    z = block(inp, z, 26, -15, 5)
+    input = @monad_input.dup
+
+    z = block(input.shift, 0, 1, 13, 5)
+    z = block(input.shift, z, 1, 15, 14)
+    z = block(input.shift, z, 1, 15, 15)
+    z = block(input.shift, z, 1, 11, 16)
+    z = block(input.shift, z, 26, -16, 8)
+    z = block(input.shift, z, 26, -11, 9)
+    z = block(input.shift, z, 26, -6, 2)
+    z = block(input.shift, z, 1, 11, 13)
+    z = block(input.shift, z, 1, 10, 16)
+    z = block(input.shift, z, 26, -10, 6)
+    z = block(input.shift, z, 26, -8, 6)
+    z = block(input.shift, z, 26, -11, 9)
+    z = block(input.shift, z, 1, 12, 11)
+    z = block(input.shift, z, 26, -15, 5)
     z
   end
 
@@ -75,6 +73,57 @@ class Alu
     z = add z, y
     z
   end
+
+  def humanize(secs)
+    [[60, :seconds], [60, :minutes], [24, :hours], [Float::INFINITY, :days]].map { |count, name|
+      if secs > 0
+        secs, n = secs.divmod(count)
+
+        "#{n.to_i} #{name}" unless n.to_i == 0
+      end
+    }.compact.reverse.join(' ')
+  end
+
+  def find
+    array = [[1, 13, 5],
+             [1, 15, 14],
+             [1, 15, 15],
+             [1, 11, 16],
+             [26, -16, 8],
+             [26, -11, 9],
+             [26, -6, 2],
+             [1, 11, 13],
+             [1, 10, 16],
+             [26, -10, 6],
+             [26, -8, 6],
+             [26, -11, 9],
+             [1, 12, 11],
+             [26, -15, 5],]
+
+    zs_to_inputs = { 0 => '' }
+    array.each do |line|
+      count = zs_to_inputs.count
+      puts "compute #{line}, #{count} sols"
+      t_start = Time.now
+      zs_to_inputs = zs_to_inputs.map do |previous_z, computed_input|
+        (1..9).map do |new_num|
+          z = block(new_num, previous_z, *line)
+          { z => (computed_input + new_num.to_s) }
+        end
+      end.flatten.uniq
+      new_zs_by_max_inputs = {}
+      zs_to_inputs.map(&:first).each do |z, computed_input|
+        new_zs_by_max_inputs[z] = computed_input if z < 1_000_000 && computed_input > (new_zs_by_max_inputs[z] || '0')
+      end
+      zs_to_inputs = new_zs_by_max_inputs
+      t_end = Time.now
+      elapsed = t_end - t_start
+      puts "\tcomputed #{line} in #{elapsed}, next estimated: #{humanize((elapsed/count) * zs_to_inputs.count)}"
+    end
+
+    puts zs_to_inputs.select { |k, v| k == 0 }
+  end
+
 end
 
 alu = Alu.new
@@ -82,6 +131,8 @@ assert_eq 2064736350, alu.valid?('13579246899999')
 alu.valid?('99999999999999')
 alu.valid?('88888888888888')
 puts "test OKs!"
+
+alu.find
 
 #
 # (11..99).to_a.find do |num|
