df59daafead564e03c33512dad6af6c9a348c3c4
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2022-12-04 23:37:15 +0100

    2015day10

---
+++ 2015/10..19/day10.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+def look_and_say(str)
+  str.chars.chunk(&:itself).map{|c, v| "#{v.size}#{c}" }.join  
+end
+
+def play_look_and_say_n_times(n, str)
+  n.times { str = look_and_say(str) }
+  str
+end
+
+input = '1321131112'
+
+puts "part1: #{play_look_and_say_n_times(40, input).size}"
