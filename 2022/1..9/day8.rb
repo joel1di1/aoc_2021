f70d5730a16249ed111f1a526cf6ade23149e790
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2022-12-08 10:14:56 +0100

    day8 part2

--- 2022/1..9/day8.rb
+++ 2022/1..9/day8.rb
@@ -8,53 +8,91 @@ end
 
 visibles = {}
 
-width = map.size
+WIDTH = map.size
 
-(0..width-1).each do |i|
+(0..WIDTH-1).each do |i|
   max_left = -1
   max_right = -1
-  (0..width-1).each do |j|
+  (0..WIDTH-1).each do |j|
     if map[i][j] > max_left
       max_left = map[i][j]
       visibles[[i, j]] = map[i][j]
     end
-    if map[i][width-1-j] > max_right
-      max_right = map[i][width-1-j]
-      visibles[[i, width-1-j]] = map[i][width-1-j]
+    if map[i][WIDTH-1-j] > max_right
+      max_right = map[i][WIDTH-1-j]
+      visibles[[i, WIDTH-1-j]] = map[i][WIDTH-1-j]
     end
   end
 end
 
-(0..width-1).each do |j|
+(0..WIDTH-1).each do |j|
   max_up = -1
   max_bottom = -1
-  (0..width-1).each do |i|
+  (0..WIDTH-1).each do |i|
     if map[i][j] > max_up
       max_up = map[i][j]
       visibles[[i, j]] = map[i][j]
     end
-    if map[width-1-i][j] > max_bottom
-      max_bottom = map[width-1-i][j]
-      visibles[[width-1-i, j]] = map[width-1-i][j]
+    if map[WIDTH-1-i][j] > max_bottom
+      max_bottom = map[WIDTH-1-i][j]
+      visibles[[WIDTH-1-i, j]] = map[WIDTH-1-i][j]
     end
   end
 end
 
 puts visibles.size
-# puts visibles.inspect
 
-new_map = Array.new(width) { Array.new }
-visibles.each do |k,v| 
-  new_map[k[0]][k[1]] = v 
-end
+# new_map = Array.new(WIDTH) { Array.new }
+# visibles.each do |k,v| 
+#   new_map[k[0]][k[1]] = v 
+# end
 
-new_map.each do |line|
-  line.each do |h|
-    putc(h.nil? ? ' ' : h.to_s)
+# new_map.each do |line|
+#   line.each do |h|
+#     putc(h.nil? ? ' ' : h.to_s)
+#   end
+#   putc "\n"
+# end
+@scenic_scores
+def scenic_score(map, coords)
+  i, j = coords
+  height = map[i][j]
+
+  top_score = 0
+  i1 = i-1
+  while i1 >= 0
+    top_score += 1
+    break if height <= map[i1][j]
+    i1 -= 1    
   end
-  putc "\n"
+
+  bottom_score = 0
+  i1 = i+1
+  while i1 < WIDTH
+    bottom_score += 1
+    break if height <= map[i1][j]
+    i1 += 1    
+  end
+
+  left_score = 0
+  j1 = j-1
+  while j1 >= 0
+    left_score += 1
+    break if height <= map[i][j1]
+    j1 -= 1    
+  end
+
+  right_score = 0
+  j1 = j+1
+  while j1 < WIDTH
+    right_score += 1
+    break if height <= map[i][j1]
+    j1 += 1    
+  end
+
+  top_score * bottom_score * left_score * right_score
 end
 
+max = (0..WIDTH-1).map { |i| (0..WIDTH-1).map{ |j| scenic_score(map, [i,j])}.max }.max
 
-# pp new_map
-# pp new_map.flatten.compact.size
+puts max
