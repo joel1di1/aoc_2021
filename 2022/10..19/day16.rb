73e3efe168863d530d39b60866b66050ec214686
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2022-12-22 23:58:50 +0100

    save fwk and day16

--- 2022/10..19/day16.rb
+++ 2022/10..19/day16.rb
@@ -2,101 +2,6 @@ require 'byebug'
 require 'set'
 require_relative '../../fwk'
 
-TOTAL_MINUTES = 30
-
-class MaxHeap
-  def initialize
-    @heap = []
-  end
-
-  def empty?
-    @heap.empty?
-  end
-
-  def size
-    @heap.length
-  end
-
-  def <<(element)
-    @heap << element
-    bubble_up(@heap.length - 1)
-  end
-
-  def pop
-    swap(0, @heap.length - 1)
-    max = @heap.pop
-    bubble_down(0)
-    max
-  end
-
-  def peek
-    @heap[0]
-  end
-
-  private
-
-  def bubble_up(index)
-    parent_index = parent_index(index)
-    return if index == 0 || @heap[parent_index] >= @heap[index]
-
-    swap(index, parent_index)
-    bubble_up(parent_index)
-  end
-
-  def bubble_down(index)
-    left_child_index = left_child_index(index)
-    right_child_index = right_child_index(index)
-    return if left_child_index >= @heap.length
-
-    if right_child_index >= @heap.length || @heap[left_child_index] >= @heap[right_child_index]
-      child_index = left_child_index
-    else
-      child_index = right_child_index
-    end
-
-    return if @heap[index] >= @heap[child_index]
-
-    swap(index, child_index)
-    bubble_down(child_index)
-  end
-
-  def swap(index1, index2)
-    @heap[index1], @heap[index2] = @heap[index2], @heap[index1]
-  end
-
-  def parent_index(index)
-    (index - 1) / 2
-  end
-
-  def left_child_index(index)
-    index * 2 + 1
-  end
-
-  def right_child_index(index)
-    index * 2 + 2
-  end
-end
-
-class PriorityQueue
-  def initialize
-    @elements = []
-  end
-
-  def push(element, priority)
-    @elements << [priority, element]
-  end
-
-  def pop
-    @elements.sort_by! { |priority, _| priority }
-    @elements.shift
-  end
-
-  def empty?
-    @elements.empty?
-  end
-end
-
-
 class Valve
   attr_reader :neighbors, :name
   attr_accessor :rate
@@ -123,7 +28,7 @@ end
 class Path
   attr_reader :closed_valves, :sequence, :remaining_steps, :confirmed_rate
 
-  def initialize(closed_valves:, sequence:, remaining_steps: TOTAL_MINUTES, confirmed_rate:)
+  def initialize(closed_valves:, sequence:, remaining_steps:, confirmed_rate:)
     @sequence = sequence
     @closed_valves = closed_valves
     @remaining_steps = remaining_steps
@@ -205,7 +110,9 @@ class Path
 
     distance_min_me = distance_to_nearest_closed_valve(last)
 
+    # debugger
     i = local_remaining_steps - distance_min_me
+
     while i > 0
       # I take the first rate
       remaining_rate += (sorted_rate.shift || 0) * (i-1)
@@ -233,7 +140,7 @@ class Path
     puts "Total pressure: #{total_pressure} - confirmed_rate: #{confirmed_rate} - #{total_pressure == confirmed_rate ? 'OK' : 'KO'}"
   end
 
-  def next_paths()
+  def next_paths(best_path)
     tmp = [] 
     # add paths open valve if not already open
     if self.last_closed?
@@ -256,7 +163,10 @@ class Path
       tmp << self.move_to(neighbor)
     end
 
-    tmp.select{|path| path.potential_rate > $best_path.confirmed_rate }
+    weird = tmp.find{|path| path.potential_rate > self.potential_rate}
+    debugger if weird
+
+    tmp.select{|path| path.potential_rate > best_path.confirmed_rate }
   end
 
   def distance_to_nearest_closed_valve(start_valve)
@@ -317,6 +227,116 @@ def init_valves
   valves
 end
 
+# puts "\tvalidation:"
+# $best_path.valid_total_rate
+
+# test potential rate
+# test_valves = [Valve.new('A', 10), Valve.new('B', 20), Valve.new('C', 30)].map{|v| [v.name, v]}.to_h
+# assert_eq 56, Path.new(closed_valves: [test_valves['B']], sequence: [test_valves['C']], remaining_steps: 0, confirmed_rate: 56).potential_rate
+# assert_eq 56, Path.new(closed_valves: [test_valves['B']], sequence: [test_valves['C']], remaining_steps: 1, confirmed_rate: 56).potential_rate
+# assert_eq 76, Path.new(closed_valves: [test_valves['B']], sequence: [test_valves['C']], remaining_steps: 2, confirmed_rate: 56).potential_rate
+# assert_eq 56, Path.new(closed_valves: [], sequence: [test_valves['C']], remaining_steps: 200, confirmed_rate: 56).potential_rate
+# assert_eq 20, Path.new(closed_valves: [test_valves['B'], test_valves['A']], sequence: [test_valves['C']], remaining_steps: 2, confirmed_rate: 0).potential_rate
+# assert_eq 40, Path.new(closed_valves: [test_valves['B'], test_valves['A']], sequence: [test_valves['C']], remaining_steps: 3, confirmed_rate: 0).potential_rate
+# assert_eq 70, Path.new(closed_valves: [test_valves['B'], test_valves['A']], sequence: [test_valves['C']], remaining_steps: 4, confirmed_rate: 0).potential_rate
+# assert_eq 40, Path.new(closed_valves: [test_valves['B'], test_valves['A']], sequence: [test_valves['B'], 'C'], remaining_steps: 3, confirmed_rate: 0).potential_rate
+
+
+# puts
+# puts Path.new(
+#   closed_valves: [valves['TG'], valves['EK'], valves['FG'], valves['EG'], valves['KR'], valves['FX'], valves['VW'], valves['HT'], valves['MS'], valves['AP'], valves['UW'], valves['TQ'], valves['WI'], valves['KS'], valves['WY']], 
+#   sequence: [valves['AA'], valves['DX'], valves['WI'], valves['EW'], valves['MT'], valves['FG']], 
+#   remaining_steps: 25, confirmed_rate: 0).potential_rate
+# puts Path.new(
+#   closed_valves: [valves['TG'], valves['EK'], valves['EG'], valves['KR'], valves['FX'], valves['VW'], valves['HT'], valves['MS'], valves['AP'], valves['UW'], valves['TQ'], valves['WI'], valves['KS'], valves['WY']], 
+#   sequence: [valves['AA'], valves['DX'], valves['WI'], valves['EW'], valves['MT'], valves['FG'], 'FG'], 
+#   remaining_steps: 24, confirmed_rate: 192).potential_rate
+# puts
+
+
+class Searcher
+  def initialize(valves, closed_valves, total_minutes)
+    @valves = valves
+    @closed_valves = closed_valves
+    @valve_aa = valves['AA']
+    @total_minutes = total_minutes
+  end
+
+  def greedy
+    # greedy algorithm
+    current_path = Path.new(closed_valves: @closed_valves, sequence: [@valve_aa], remaining_steps: TOTAL_MINUTES, confirmed_rate: 0)
+
+    while current_path.remaining_steps > 0
+      # move me
+      # if current valve is closed, open it
+      if current_path.last_closed?
+        current_path = current_path.open_last 
+      else
+        # if current valve is open, move to max rated neighbor
+        next_neighbor = current_path.last.neighbors.select { |n| current_path.closed_valves.include?(n) }.sort_by(&:rate).last
+        next_neighbor ||= current_path.last.neighbors.sample
+        current_path = current_path.move_to(next_neighbor)
+      end
+    end
+    current_path
+  end
+
+  def n_greedy(n = 100)
+    best_path = nil
+    n.times do 
+      current_path = greedy
+      best_path = current_path if best_path.nil? || current_path.confirmed_rate > best_path.confirmed_rate
+    end
+    best_path
+  end
+
+  # A* algorithm
+  def a_star
+    paths_to_explore = MaxHeap.new
+
+    start = Path.new(closed_valves: @closed_valves, sequence: [@valve_aa], remaining_steps: @total_minutes, confirmed_rate: 0)
+    paths_to_explore << start
+
+    best_path = n_greedy
+
+    iter = 0
+    while !paths_to_explore.empty? 
+      # paths_to_explore.each do |path|
+      #   puts "\t #{path}"
+      # end
+      
+      current_path = paths_to_explore.pop
+      # if iter % 20_000 == 0
+      #   puts "iter: #{iter}, paths to explore: #{paths_to_explore.size}, $best_path: #{best_path.confirmed_rate}" 
+      #   puts "-- exploring #{current_path}"
+      # end
+      
+      iter+=1
+      # if current_path.potential_rate <= $best_path.confirmed_rate
+      #   puts "path #{current_path} is not worth exploring"
+      #   break
+      # end
+      
+      if current_path.confirmed_rate > best_path.confirmed_rate
+        best_path = current_path
+        # clear paths to explore
+        # paths_to_explore = paths_to_explore.select{|p| p.potential_rate > best_path.confirmed_rate }
+      end
+        
+      # add child paths
+      current_path.next_paths(best_path).each do |next_path|
+        paths_to_explore << next_path
+      end
+    end
+
+    # puts "best path: #{best_path}"
+    # puts "\tvalidation:"
+    # best_path.valid_total_rate
+    # puts "\n\t==> best confirmed rate: #{best_path.confirmed_rate}\n"
+    best_path
+  end
+end
+
 
 valves = init_valves
 valve_aa = valves['AA']
@@ -325,97 +345,36 @@ valve_aa = valves['AA']
 closed_valves = valves.values.select{|v| v.rate > 0}.freeze
 puts "closed_valves: #{closed_valves.length}"
 
-$best_path = nil
+searcher = Searcher.new(valves, closed_valves, 30)
 
-def greedy(closed_valves, valve_aa)
-  # greedy algorithm
-  current_path = Path.new(closed_valves: closed_valves, sequence: [valve_aa], remaining_steps: TOTAL_MINUTES, confirmed_rate: 0)
-
-  while current_path.remaining_steps > 0
-    # move me
-    # if current valve is closed, open it
-    if current_path.last_closed?
-      current_path = current_path.open_last 
-    else
-      # if current valve is open, move to max rated neighbor
-      next_neighbor = current_path.last.neighbors.select { |n| current_path.closed_valves.include?(n) }.sort_by(&:rate).last
-      next_neighbor ||= current_path.last.neighbors.sample
-      current_path = current_path.move_to(next_neighbor)
-    end
-  end
-  current_path
-end
+puts "part1: #{searcher.a_star.confirmed_rate}"
 
 
-100.times do 
-  current_path = greedy(closed_valves, valve_aa)
-  $best_path = current_path if $best_path.nil? || current_path.confirmed_rate > $best_path.confirmed_rate
-end
 
-puts "greedy: #{$best_path}"
-# puts "\tvalidation:"
-# $best_path.valid_total_rate
+# part 2
 
-# test potential rate
-test_valves = [Valve.new('A', 10), Valve.new('B', 20), Valve.new('C', 30)].map{|v| [v.name, v]}.to_h
-assert_eq 56, Path.new(closed_valves: [test_valves['B']], sequence: [test_valves['C']], remaining_steps: 0, confirmed_rate: 56).potential_rate
-assert_eq 56, Path.new(closed_valves: [test_valves['B']], sequence: [test_valves['C']], remaining_steps: 1, confirmed_rate: 56).potential_rate
-assert_eq 76, Path.new(closed_valves: [test_valves['B']], sequence: [test_valves['C']], remaining_steps: 2, confirmed_rate: 56).potential_rate
-assert_eq 56, Path.new(closed_valves: [], sequence: [test_valves['C']], remaining_steps: 200, confirmed_rate: 56).potential_rate
-assert_eq 20, Path.new(closed_valves: [test_valves['B'], test_valves['A']], sequence: [test_valves['C']], remaining_steps: 2, confirmed_rate: 0).potential_rate
-assert_eq 40, Path.new(closed_valves: [test_valves['B'], test_valves['A']], sequence: [test_valves['C']], remaining_steps: 3, confirmed_rate: 0).potential_rate
-assert_eq 70, Path.new(closed_valves: [test_valves['B'], test_valves['A']], sequence: [test_valves['C']], remaining_steps: 4, confirmed_rate: 0).potential_rate
+total_pos = (4..(closed_valves.length/2)).to_a.map { |s| closed_valves.combination(s).count }.sum
+puts "total possible combinations: #{total_pos}"
 
-
-mh = MaxHeap
-10_000.times do
-  mh.push(Random.rand(100))
-end
-
-cur = mh.pop
-while !mh.empty?
-  next_val = mh.pop
-  assert cur >= next_val
-  cur = next_val
-end
-
-
-# # A* algorithm
-paths_to_explore = MaxHeap.new
-
-start = Path.new(closed_valves: closed_valves, sequence: [valve_aa], remaining_steps: TOTAL_MINUTES, confirmed_rate: 0)
-paths_to_explore << start
-
+# create all possible s way to split the closed valves into 2 groups
+best_value = 0
 iter = 0
-while !paths_to_explore.empty? 
-  # paths_to_explore.each do |path|
-  #   puts "\t #{path}"
-  # end
-  
-  current_path = paths_to_explore.pop
-  if iter % 1_000 == 0
-    puts "iter: #{iter}, paths to explore: #{paths_to_explore.size}, $best_path: #{$best_path.confirmed_rate}" 
-    puts "-- exploring #{current_path}"
-  end
-  
-  iter+=1
-  # if current_path.potential_rate <= $best_path.confirmed_rate
-  #   puts "path #{current_path} is not worth exploring"
-  #   break
-  # end
-  
-  if current_path.confirmed_rate > $best_path.confirmed_rate
-    $best_path = current_path
-    # clear paths to explore
-    # paths_to_explore = paths_to_explore.select{|p| p.potential_rate > $best_path.confirmed_rate }
-  end
-    
-  # add child paths
-  current_path.next_paths.each do |next_path|
-    paths_to_explore << next_path
+
+started_at = Time.now
+
+4.upto(closed_valves.length/2) do |s|
+  closed_valves.combination(s).each do |group1|
+    group2 = closed_valves - group1
+    sum = [group1, group2].map do |group|
+      searcher = Searcher.new(valves, group1, 26)
+      searcher.a_star.confirmed_rate
+    end.sum
+    best_value = sum if sum > best_value
+    if iter % 10 == 0
+      puts "iter: #{iter}, best_value: #{best_value}, remaining: #{total_pos - iter}"
+      puts "elapsed: #{Time.now - started_at}"
+      puts "remaining: #{(Time.now - started_at) * (total_pos - iter) / iter}"
+    end
+    iter += 1
   end
 end
-
-puts "best path: #{$best_path}"
-puts "\tvalidation:"
-$best_path.valid_total_rate
--- fwk.rb
+++ fwk.rb
@@ -12,3 +12,148 @@ def assert(actual, msg: nil)
   msg ||= "Expected truthy but received #{actual}"
   raise msg unless actual
 end
+
+class Heap
+  def initialize
+    @heap = []
+  end
+
+  def push(element)
+    @heap << element
+    bubble_up(@heap.length - 1)
+  end
+
+  def pop
+    swap(0, @heap.length - 1)
+    min = @heap.pop
+    bubble_down(0)
+    min
+  end
+
+  def peek
+    @heap[0]
+  end
+
+  private
+
+  def bubble_up(index)
+    parent_index = parent_index(index)
+    return if index == 0 || compare(@heap[parent_index], @heap[index])
+
+    swap(index, parent_index)
+    bubble_up(parent_index)
+  end
+
+  def bubble_down(index)
+    left_child_index = left_child_index(index)
+    right_child_index = right_child_index(index)
+    return if left_child_index >= @heap.length
+
+    if right_child_index >= @heap.length || compare(@heap[left_child_index], @heap[right_child_index])
+      child_index = left_child_index
+    else
+      child_index = right_child_index
+    end
+
+    return if compare(@heap[index], @heap[child_index])
+
+    swap(index, child_index)
+    bubble_down(child_index)
+  end
+
+  def swap(index1, index2)
+    @heap[index1], @heap[index2] = @heap[index2], @heap[index1]
+  end
+
+  def parent_index(index)
+    (index - 1) / 2
+  end
+
+  def left_child_index(index)
+    index * 2 + 1
+  end
+
+  def right_child_index(index)
+    index * 2 + 2
+  end
+end
+
+class MaxHeap < Heap
+  def compare(a, b)
+    a >= b
+  end
+end
+
+class MinHeap < Heap
+  def compare(a, b)
+    a <= b
+  end
+end
+
+class HeapElement
+  def initialize(value, priority)
+    @value = value
+    @priority = priority
+  end
+
+  def <=>(other)
+    @priority <=> other.priority
+  end
+
+  def <=(other)
+    @priority <= other.priority
+  end
+
+  def >=(other)
+    @priority >= other.priority
+  end
+    
+  def <(other)
+    @priority < other.priority
+  end
+
+  def >(other)
+    @priority > other.priority
+  end
+end
+
+# find the shortest path between two nodes in a graph
+# Nodes must respond to #neighbors and #cost(neighbor)
+def dijkstra(start_node, end_node)
+  # Create a set to store the nodes that have been visited
+  visited = Set.new
+
+  # Create a priority queue to store the nodes that need to be processed
+  # The priority queue will be sorted by the distance from the start node
+  pq = MinHeap.new
+
+  # Add the start node to the priority queue with a distance of 0
+  pq.push(start_node, 0)
+
+  # While there are nodes in the priority queue
+  while !pq.empty?
+    # Get the node with the smallest distance from the priority queue
+    current_node, distance = pq.pop
+
+    # If the current node is the end node, return the distance
+    return distance if current_node == end_node
+
+    # Mark the current node as visited
+    visited.add(current_node)
+
+    # For each neighbor of the current node
+    current_node.neighbors.each do |neighbor|
+      # If the neighbor has not been visited
+      if !visited.include?(neighbor)
+        # Calculate the distance to the neighbor as the distance to the current node plus the cost of the edge between the current node and the neighbor
+        neighbor_distance = distance + current_node.cost(neighbor)
+
+        # Add the neighbor to the priority queue with the calculated distance
+        pq.push(neighbor, neighbor_distance)
+      end
+    end
+  end
+
+  # If the end node was not reached, return nil
+  nil
+end
