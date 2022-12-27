b5004dafb900cd7a1dd0b6e57e8149e190040457
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2022-12-27 19:33:42 +0100

    day2_openai_part2.rb

--- 2022/1..9/day2_openai_part1.rb
+++ 2022/1..9/day2_openai_part1.rb
@@ -1,46 +1,42 @@
-# Read the input file and parse it
-def read_and_parse_input_file(filename)
-  input = File.readlines(filename).map(&:strip)
-  input.map do |line|
-    {
-      opponent: line[0],
-      response: line[2]
-    }
+# Read the input file and parse the strategy guide
+strategy_guide = File.readlines("day2.txt").map { |line| line.chomp.split(" ") }
+
+# Initialize the total score
+total_score = 0
+
+# Hash to map shapes to numbers
+SHAPE_MAP = {
+  "A" => 1,
+  "B" => 2,
+  "C" => 3,
+  "X" => 1,
+  "Y" => 2,
+  "Z" => 3
+}
+
+# Iterate through each round of the game
+strategy_guide.each do |opponent_shape, your_shape|
+  # Initialize the score for the round
+  score = 0
+
+  # Calculate the score for the round
+  case SHAPE_MAP[your_shape] - SHAPE_MAP[opponent_shape]
+  when -2, 1
+    # You win
+    score = 6
+  when -1, 2
+    # You lose
+    score = 0
+  when 0
+    # The round is a draw
+    score = 3
   end
+  # Add the value corresponding to the shape that you selected to the score
+  score += SHAPE_MAP[your_shape]
+
+  # Add the score for the round to the total score
+  total_score += score
 end
 
-# Calculate the score for a single round
-def calculate_round_score(round)
-  shape = case round[:response]
-          when "X" then 1
-          when "Y" then 2
-          when "Z" then 3
-          end
-
-  outcome = if round[:opponent] == "A" && round[:response] == "Y" ||
-              round[:opponent] == "B" && round[:response] == "Z" ||
-              round[:opponent] == "C" && round[:response] == "X"
-              6
-            elsif round[:opponent] == "A" && round[:response] == "X" ||
-                  round[:opponent] == "B" && round[:response] == "Y" ||
-                  round[:opponent] == "C" && round[:response] == "Z"
-                  3
-            else
-              0
-            end
-
-  shape + outcome
-end
-
-# Calculate the total score for a list of rounds
-def calculate_total_score(rounds)
-  rounds.map { |round| calculate_round_score(round) }.sum
-end
-
-# Main function
-def main
-  input = read_and_parse_input_file("day2.txt")
-  puts "Total score: #{calculate_total_score(input)}"
-end
-
-main
+# Print the final total score
+puts total_score
--- 2022/1..9/day2_openai_part2.rb
+++ 2022/1..9/day2_openai_part2.rb
@@ -1,51 +1,48 @@
-require 'byebug'
-# Read the input file and parse it
-def read_and_parse_input_file(filename)
-  input = File.readlines(filename).map(&:strip)
-  input.map do |line|
-    {
-      outcome: line[2],
-      opponent: line[0]
-    }
+# Read the input file and parse the strategy guide
+strategy_guide = File.readlines("day2.txt").map { |line| line.chomp.split(" ") }
+
+# Initialize the total score
+total_score = 0
+
+# Hash to map opponent shapes to numbers
+SHAPE_MAP = {
+  "A" => 1,
+  "B" => 2,
+  "C" => 3
+}
+
+# Hash to map desired outcomes to numbers
+OUTCOME_MAP = {
+  "X" => 0,
+  "Y" => 3,
+  "Z" => 6
+}
+
+# Iterate through each round of the game
+strategy_guide.each do |opponent_shape, desired_outcome|
+  # Initialize the score for the round
+  score = 0
+
+  # Determine the shape that you need to play in order to achieve the desired outcome
+  case desired_outcome
+  when "X"
+    your_shape = ((SHAPE_MAP[opponent_shape] - 1 + (-1)) % 3) + 1
+  when "Y"
+    your_shape = ((SHAPE_MAP[opponent_shape] - 1 + (+0)) % 3) + 1
+  when "Z"
+    your_shape = ((SHAPE_MAP[opponent_shape] - 1 + (+1)) % 3) + 1
   end
+
+  # Calculate the scores for the outcome and the shape
+  outcome_score = OUTCOME_MAP[desired_outcome]
+  shape_score = your_shape
+
+  # Add the scores to the score for the round
+  score = outcome_score + shape_score
+
+  # Add the score for the round to the total score
+  total_score += score
 end
 
-# Calculate the score for a single round
-def calculate_round_score(round)
-  # Determine the shape to play
-  shape = case round[:opponent]
-          when "A" then round[:outcome] == "X" ? "C" : round[:outcome] == "Z" ? "B" : "A"
-          when "B" then round[:outcome] == "X" ? "A" : round[:outcome] == "Y" ? "B" : "C"
-          when "C" then round[:outcome] == "X" ? "B" : round[:outcome] == "Y" ? "C" : "A"
-          end
-
-  # Determine the score for the shape
-  shape_score = case shape
-                when "A" then 1
-                when "B" then 2
-                when "C" then 3
-                end
-
-  # Determine the outcome of the round
-  outcome = case round[:outcome]
-            when "X" then 0
-            when "Y" then 3
-            when "Z" then 6
-            end
-
-  # Return the total score for the round
-  shape_score + outcome
-end
-
-# Calculate the total score for a list of rounds
-def calculate_total_score(rounds)
-  rounds.map { |round| calculate_round_score(round) }.sum
-end
-
-# Main function
-def main
-  input = read_and_parse_input_file("day2.txt")
-  puts "Total score: #{calculate_total_score(input)}"
-end
-
-main
+# Print the final total score
+puts total_score
