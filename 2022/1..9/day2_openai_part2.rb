9e56f802d0f1d7516b529b66921792a3f216fd74
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2022-12-27 19:36:04 +0100

    day2_openai_part2.rb

--- 2022/1..9/day2_openai_part2.rb
+++ 2022/1..9/day2_openai_part2.rb
@@ -20,24 +20,14 @@ OUTCOME_MAP = {
 
 # Iterate through each round of the game
 strategy_guide.each do |opponent_shape, desired_outcome|
-  # Initialize the score for the round
-  score = 0
-
   # Determine the shape that you need to play in order to achieve the desired outcome
-  case desired_outcome
-  when "X"
-    your_shape = ((SHAPE_MAP[opponent_shape] - 1 + (-1)) % 3) + 1
-  when "Y"
-    your_shape = ((SHAPE_MAP[opponent_shape] - 1 + (+0)) % 3) + 1
-  when "Z"
-    your_shape = ((SHAPE_MAP[opponent_shape] - 1 + (+1)) % 3) + 1
-  end
+  your_shape = ((SHAPE_MAP[opponent_shape] - 1 + OUTCOME_MAP[desired_outcome]) % 3) + 1
 
   # Calculate the scores for the outcome and the shape
   outcome_score = OUTCOME_MAP[desired_outcome]
   shape_score = your_shape
 
-  # Add the scores to the score for the round
+  # Calculate the score for the round
   score = outcome_score + shape_score
 
   # Add the score for the round to the total score
