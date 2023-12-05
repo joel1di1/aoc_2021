bfc41dec1e9792306dedae4cfd90efde82e820a9
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2023-12-05 23:52:28 +0100

    2023d5

--- 2023/0..9/day3.rb
+++ 2023/0..9/day3.rb
@@ -28,7 +28,6 @@ def part_number?(first_num, chars, i, j)
 end
 
 
-
 lines = File.readlines('input3.txt').map(&:strip)
 
 chars = lines.map(&:chars).freeze
@@ -50,3 +49,58 @@ sum = 0
 end
 
 puts "part1: #{sum}"
+
+
+sum_gear_ratio = 0
+
+stars = Set.new
+# collect stars in the chars grid
+(0..chars.size - 1).map do |i|
+  (0..chars[i].size - 1).map do |j|
+    stars << [i, j] if chars[i][j] == '*'
+  end
+end
+
+part_of_stars = {}
+
+(0..chars.size - 1).map do |i|
+  j = 0
+  while j < chars[i].size
+    first_num = lines[i..][0][j..][/^\d+/]
+
+    if first_num
+      # for all positions around the number
+      # if the position is a star, add the number to the part of the star
+      (j-1..j+first_num.size).each do |k|
+        if stars.include?([i-1, k])
+          part_of_stars[[i-1, k]] ||= []
+          part_of_stars[[i-1, k]] << first_num.to_i
+        end
+        if stars.include?([i+1, k])
+          part_of_stars[[i+1, k]] ||= []
+          part_of_stars[[i+1, k]] << first_num.to_i
+        end
+      end
+
+      if stars.include?([i, j-1])
+        part_of_stars[[i, j-1]] ||= []
+        part_of_stars[[i, j-1]] << first_num.to_i
+      end
+
+      if stars.include?([i, j+first_num.size])
+        part_of_stars[[i, j+first_num.size]] ||= []
+        part_of_stars[[i, j+first_num.size]] << first_num.to_i
+      end
+
+      j += first_num.size
+    else
+      j += 1
+    end
+  end
+end
+
+part_of_stars.map do |star, parts|
+  sum_gear_ratio += parts.reduce(:*) if parts.size == 2
+end
+
+puts "part2: #{sum_gear_ratio}"
---
+++ 2023/0..9/day4.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require 'readline'
+require 'byebug'
+require 'set'
+require_relative '../../fwk'
+
+lines = File.readlines('input4.txt').map(&:strip)
+
+worths = lines.map do |line|
+  _left, right = line.split(':')
+  wining_numbers, my_numbers = right.split('|').map(&:strip)
+
+  wining_numbers = wining_numbers.split(' ').map(&:to_i)
+  wining_numbers_set = Set.new(wining_numbers)
+  my_numbers = my_numbers.split(' ').map(&:to_i)
+
+  points = 0
+  my_numbers.each do |my_number|
+    if wining_numbers_set.include?(my_number)
+      if points == 0
+        points = 1
+      else
+        points *= 2
+      end
+    end
+  end
+  points
+end
+
+puts "part1: #{worths.sum}"
+
+
+card_wins = {}
+lines.map.with_index do |line, i|
+  _left, right = line.split(':')
+  wining_numbers, my_numbers = right.split('|').map(&:strip)
+
+  wining_numbers = wining_numbers.split(' ').map(&:to_i)
+  wining_numbers_set = Set.new(wining_numbers)
+  my_numbers = my_numbers.split(' ').map(&:to_i)
+
+  # numbers of my numbers that are winning numbers
+  my_winning_numbers = my_numbers.select { |my_number| wining_numbers_set.include?(my_number) }
+
+  card_wins[i] = [my_winning_numbers.count, 1]
+end
+
+i = 0
+while i < card_wins.size
+  nb_win, nb_cards = card_wins[i]
+  if nb_win > 0
+    (1..nb_win).each do |j|
+      card_wins[i + j][1] += nb_cards if i + j < card_wins.size
+    end
+  end
+  i+=1
+end
+
+puts "part2: #{card_wins.values.map{|c| c[1]}.sum }}"
