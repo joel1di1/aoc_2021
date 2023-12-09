95c7d5de9a873fb8e4f712cd5e28a25e6a7f8c00
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2023-12-09 01:13:55 +0100

    save

--- 2023/0..9/day8.rb
+++ 2023/0..9/day8.rb
@@ -23,7 +23,7 @@ end.to_h.freeze
 
 
 class Ghost
-  attr_reader :name, :path, :visited
+  attr_reader :name, :path, :visited, :loop, :finishing_nodes, :first_finishing_index, :loop_size, :shift_for_loop
 
   def initialize(name)
     @name = name
@@ -31,75 +31,133 @@ class Ghost
     @path << [name, 0]
     @visited = {}
     @visited[[name, 0]] = 0
+    @loop = false
+    @shift_for_loop = 0
   end
 
   def next(step)
+    # puts "ghost #{name} at step #{step}"
+
     mod = step % SEQUENCE.size
     dir = SEQUENCE[mod]
     next_node = NODES[@path.last[0]][dir == 'L' ? 0 : 1]
+
+    # puts "#{name} at step #{step} (#{dir}) goes from #{@path[-1].first} (#{NODES[@path.last[0]]}) to #{next_node}"
+
+    visited_index = @visited[[next_node, mod]]
+    if visited_index
+      @loop = true
+
+      @shift_for_loop = visited_index
+      finisher_index = @path.index(@path.select { |p| p[0].end_with?('Z') }.first)
+      # puts "#{@path.select { |p| p[0].end_with?('Z') }.first}, finisher_index: #{finisher_index}, path: #{@path}"
+
+      # find the node where the loop starts
+      @path = @path[visited_index..]
+      # in that new path, find the node with a name that ends with Z
+      @finishing_nodes = @path.select { |p| p[0].end_with?('Z') }
+
+      @loop_size = @path.size
+
+      # puts "loop detected for ghost #{name}: #{[next_node, mod]}, loop start index: #{visited_index}, loop size: #{path.size}, loop starting at #{path.first}, ending at #{path.last}, loop: #{@path}"
+      # puts "\t finisher index: #{finisher_index}"
+      # puts "\t finishing nodes: #{finishing_nodes}"
+
+      @first_finishing_index = @path.index(@finishing_nodes.first) + step + 1
+      # puts "\t first finishing index: #{first_finishing_index}"
+    else
+      @path << [next_node, mod]
+      @visited[[next_node, mod]] = @path.size - 1
+    end
+  end
+
+  def next_force(step)
+    mod = step % SEQUENCE.size
+    dir = SEQUENCE[mod]
+    next_node = NODES[@path.last[0]][dir == 'L' ? 0 : 1]
+
+    puts "#{name} at step #{step} finishes at #{next_node}" if next_node.end_with?('Z')
+
+    # puts "#{name} at step #{step} goes to #{next_node}"
+
+    # puts "#{name} found Z at step #{step}" if next_node.end_with?('Z')
+
     @path << [next_node, mod]
-    @visited[[next_node, mod]] = @path.size - 1
-  end
-end
-
-class Node
-  attr_accessor :name, :mod
-
-  def initialize(name, mod)
-    @name = name
-    @mod = mod
-  end
-
-  def <=>(other)
-    res = @name <=> other.name
-    return res unless res == 0
-
-    @mod <=> other.mod
-  end
-
-  def ==(other)
-    @name == other.name && @mod == other.mod
   end
 
   def to_s
-    "#{@name}-#{@mod}"
+    "#{name}(loop_size: #{loop_size}, shift_for_loop: #{shift_for_loop})"
+  end
+
+  def looped?
+    @loop
+  end
+
+  def winning?(steps)
+    # puts "\t #{name} steps - @shift_for_loop: #{@shift_for_loop}, loop_size: #{@loop_size}, mod: #{(steps) % @loop_size}"
+    (steps) % @loop_size == 0
   end
 end
 
-positions = NODES.keys.select { |k| k.end_with?('A') }
+starting_positions = NODES.keys.select { |k| k.end_with?('A') }
 
 set = Set.new
 
-puts "starting positions: #{positions}"
+ghosts = starting_positions.map { |p| Ghost.new(p) }
 
-travels = positions.map { |p| Set.new([p, 0]) }
-loops = {}
+puts "starting ghost: #{ghosts.map(&:to_s).join(', ')}"
 
 steps = 0
-until positions.all? { |p| p.end_with?('Z') }
-  mod = steps % SEQUENCE.size
-  dir = SEQUENCE[mod]
 
-  old_positions = positions
-  positions = positions.map.with_index do |position, index|
-    if loops[index].nil?
-      node_key = [position, mod]
-      if travels[index].include?(node_key)
-        puts "loop detected for ghost ##{index}: #{node_key} - index: #{steps}"
-        loops[index] = true
-      else
-        travels[index] << node_key
-      end
-    end
-
-    next_node = dir == 'L' ? NODES[position][0] : NODES[position][1]
-
-    next_node
+until ghosts.all?(&:looped?)
+  ghosts.reject(&:looped?).each do |ghost|
+    ghost.next(steps)
   end
-
-  puts "#{steps}: mod(#{mod}) #{dir} #{old_positions} => #{positions}" if loops.size == 6
 
   steps += 1
 end
 
-puts "Part2: #{steps}"
+test_ghosts = ghosts.map { |g| Ghost.new(g.name) }
+steps = 0
+
+# for each ghost, make test_ghost go to first_finishing_index and chek if the resulting position ends with 'Z'
+ghosts.each_with_index do |ghost, index|
+  test_ghost = test_ghosts[index]
+  step = 0
+
+  until step == (ghost.first_finishing_index + (5*ghost.loop_size))
+    test_ghost.next_force(step)
+    step += 1
+  end
+
+  # puts test_ghost.path.inspect
+
+  if test_ghost.path.last[0].end_with?('Z')
+    puts "step #{step} is winning step for ghost #{ghost}"
+  else
+    puts "step #{step} is losing step for ghost #{ghost}"
+    # puts "ghost path: #{ghost.path}"
+  end
+end
+
+# find ghost that have the highest loop size
+ghost_by_loop_size_desc = ghosts.sort_by(&:loop_size).reverse
+ghost_with_max_loop_size = ghost_by_loop_size_desc.first
+
+puts "ghost with max loop size: #{ghost_with_max_loop_size}, loop size: #{ghost_with_max_loop_size.loop_size}, first_finishing_index: #{ghost_with_max_loop_size.first_finishing_index}"
+
+steps = 95081082247146207
+puts "steps: #{steps} loop_size: #{ghost_with_max_loop_size.loop_size}, winning? #{ghost_with_max_loop_size.winning?(steps)}"
+
+until ghosts.select { |ghost| ghost.winning?(steps) }.count == ghosts.count
+  count = ghosts.select { |ghost| ghost.winning?(steps) }.count
+  if count > 5
+    puts "match: #{count}}"
+    puts "steps: #{steps}"
+  end
+  steps += 4726811977594001
+  # puts "steps: #{steps}"
+end
+
+
+puts "winning steps: #{steps}"
---
+++ 2023/0..9/day8b.rb
@@ -0,0 +1,69 @@
+class Map
+  attr_accessor :directions, :nodes
+
+  def initialize
+    @directions = []
+    @nodes = {}
+  end
+
+  def starting_nodes
+    @nodes.values.select(&:starting_node?)
+  end
+
+  def steps
+    starting_nodes.map { |node| node.steps(@directions) }.reduce { |lcm, count| lcm.lcm(count) }
+  end
+end
+
+class Node
+  attr_accessor :name, :left, :right
+
+  def initialize(name)
+    @name = name
+  end
+
+  def starting_node?
+    @name[-1] == 'A'
+  end
+
+  def ending_node?
+    @name[-1] == 'Z'
+  end
+
+  def follow(direction)
+    direction == 'L' ? left : right
+  end
+
+  def steps(directions)
+    steps = 0
+    curr = self
+    iterator = directions.cycle
+    until curr.ending_node?
+      steps += 1
+      curr = curr.follow(iterator.next)
+    end
+    steps
+  end
+
+  def inspect
+    "#{@name} (#{@left.try(&:name)}, #{@right.try(&:name)})"
+  end
+end
+
+map = Map.new
+
+lines = File.readlines("#{__dir__}/input8.txt", chomp: true)
+map.directions = lines.first.chars
+
+lines[2..].each do |line|
+  name, left, right = line.split(/\W+/)
+
+  map.nodes[name] ||= Node.new(name)
+  map.nodes[left] ||= Node.new(left)
+  map.nodes[right] ||= Node.new(right)
+
+  map.nodes[name].left = map.nodes[left]
+  map.nodes[name].right = map.nodes[right]
+end
+
+puts map.steps
