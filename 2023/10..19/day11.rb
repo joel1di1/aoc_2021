2fad8ed527d0aae27c9b17e6474b731371e8041f
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2023-12-12 00:24:19 +0100

    2023d211part2

--- 2023/10..19/day11.rb
+++ 2023/10..19/day11.rb
@@ -15,7 +15,7 @@ File.open('input11.txt').each do |line|
 end
 
 # print the grid
-grid.each { |row| puts row.join }
+# grid.each { |row| puts row.join }
 
 expand_row_index = []
 expand_col_index = []
@@ -28,20 +28,25 @@ end
   expand_col_index << col_index if grid.all? { |row| row[col_index] == '.' }
 end
 
-puts 'after inserting rows'
-# print the grid
-grid.each do |row|
-  puts row.join
-end
-
-
 stars = []
 # find all '#'
-grid.each_with_index do |row, y|
-  row.each_with_index do |col, x|
+grid.each_with_index do |row, x|
+  row.each_with_index do |col, y|
     stars << [x, y] if col == '#'
   end
 end
+
+# puts 'after inserting rows'
+# # print the grid
+# grid.each_with_index do |row, i|
+#   row.each_with_index do |char, j|
+#     print char == '#' ? stars.index([i, j]) : char
+#   end
+#   puts
+# end
+
+
+EXPANSION = 1000000
 
 lengh_sum = stars[..-2].map.with_index do |star, index|
   stars[index + 1..].map do |other_star|
@@ -51,11 +56,11 @@ lengh_sum = stars[..-2].map.with_index do |star, index|
     distance = vector[0].abs + vector[1].abs
 
     # count how many time we cross a expanded row or column
-    expand_rows = expand_row_index.select { |row_index| row_index.between?(star[0], other_star[0]) }.count
-    expand_cols = expand_col_index.select { |col_index| col_index.between?(star[1], other_star[1]) }.count
+    expand_rows = expand_row_index.select { |row_index| row_index.between?([star[0], other_star[0]].min, [star[0], other_star[0]].max) }.count
+    expand_cols = expand_col_index.select { |col_index| col_index.between?([star[1], other_star[1]].min, [star[1], other_star[1]].max) }.count
 
-    total = distance + expand_rows + expand_cols
-    puts "star: #{stars.index(star)}, other_star: #{stars.index(other_star)}, distance: #{distance}, expand_rows: #{expand_rows}, expand_cols: #{expand_cols}, total: #{total}"
+    total = distance + (expand_rows + expand_cols) * (EXPANSION - 1)
+    # puts "star: #{stars.index(star)}, other_star: #{stars.index(other_star)}, distance: #{distance}, expand_rows: #{expand_rows}, expand_cols: #{expand_cols}, total: #{total}"
     total
   end
 end.flatten.sum
