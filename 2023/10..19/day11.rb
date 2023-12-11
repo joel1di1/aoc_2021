7e54962bd6c8378d716b0c2d4f119496f4bab4d9
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2023-12-12 00:32:13 +0100

    smaller_code

--- 2023/10..19/day11.rb
+++ 2023/10..19/day11.rb
@@ -6,27 +6,10 @@ require 'byebug'
 require 'set'
 require_relative '../../fwk'
 
-# read input11.txt and construct a grid with the chars
+grid = File.readlines("#{__dir__}/input11.txt", chomp: true).map(&:chars)
 
-grid = []
-
-File.open('input11.txt').each do |line|
-  grid << line.chomp.chars
-end
-
-# print the grid
-# grid.each { |row| puts row.join }
-
-expand_row_index = []
-expand_col_index = []
-
-(0...grid.size).each do |row_index|
-  expand_row_index << row_index if grid[row_index].all? { |c| c == '.' }
-end
-
-(0...grid[0].size).each do |col_index|
-  expand_col_index << col_index if grid.all? { |row| row[col_index] == '.' }
-end
+expand_row_index = (0...grid.size).select { |row_index| grid[row_index].all? { |c| c == '.' } }
+expand_col_index = (0...grid[0].size).select { |col_index| grid.all? { |row| row[col_index] == '.' } }
 
 stars = []
 # find all '#'
@@ -36,33 +19,21 @@ grid.each_with_index do |row, x|
   end
 end
 
-# puts 'after inserting rows'
-# # print the grid
-# grid.each_with_index do |row, i|
-#   row.each_with_index do |char, j|
-#     print char == '#' ? stars.index([i, j]) : char
-#   end
-#   puts
-# end
+def total_distance(stars, expand_row_index, expand_col_index, expansion)
+  lengh_sum = stars[..-2].map.with_index do |star, index|
+    stars[index + 1..].map do |other_star|
+      distance = (other_star[0] - star[0]).abs + (other_star[1] - star[1]).abs
 
+      # count how many time we cross a expanded row or column
+      expand_rows = expand_row_index.select { |row_index| row_index.between?([star[0], other_star[0]].min, [star[0], other_star[0]].max) }.count
+      expand_cols = expand_col_index.select { |col_index| col_index.between?([star[1], other_star[1]].min, [star[1], other_star[1]].max) }.count
 
-EXPANSION = 1000000
+      total = distance + (expand_rows + expand_cols) * (expansion - 1)
+      # puts "star: #{stars.index(star)}, other_star: #{stars.index(other_star)}, distance: #{distance}, expand_rows: #{expand_rows}, expand_cols: #{expand_cols}, total: #{total}"
+      total
+    end
+  end.flatten.sum
+end
 
-lengh_sum = stars[..-2].map.with_index do |star, index|
-  stars[index + 1..].map do |other_star|
-    # find the vector between the two stars
-    vector = [other_star[0] - star[0], other_star[1] - star[1]]
-    # find the distance between the two stars
-    distance = vector[0].abs + vector[1].abs
-
-    # count how many time we cross a expanded row or column
-    expand_rows = expand_row_index.select { |row_index| row_index.between?([star[0], other_star[0]].min, [star[0], other_star[0]].max) }.count
-    expand_cols = expand_col_index.select { |col_index| col_index.between?([star[1], other_star[1]].min, [star[1], other_star[1]].max) }.count
-
-    total = distance + (expand_rows + expand_cols) * (EXPANSION - 1)
-    # puts "star: #{stars.index(star)}, other_star: #{stars.index(other_star)}, distance: #{distance}, expand_rows: #{expand_rows}, expand_cols: #{expand_cols}, total: #{total}"
-    total
-  end
-end.flatten.sum
-
-puts "part1: #{lengh_sum}"
+puts "part1: #{total_distance(stars, expand_row_index, expand_col_index, 2)}"
+puts "part1: #{total_distance(stars, expand_row_index, expand_col_index, 1000000)}"
