8cf3b1282f3b3ecd908d52e21b6f2aa8273d75cd
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2024-12-01 10:17:41 +0100

    clean code

--- 2024/01..09/day1.rb
+++ 2024/01..09/day1.rb
@@ -1,38 +1,13 @@
-require 'byebug'
-require 'set'
 require_relative '../../fwk'
 
 lines = File.readlines(File.join(__dir__, 'input1.txt'))
 
-lefts = []
-rights = []
+lefts, rights = lines.map { |line| line.split.map(&:to_i) }.transpose
 
-lines.each do |line|
-  left, right = line.split(' ')
-  lefts << left.to_i
-  rights << right.to_i
-end
-
-lefts_sorted = lefts.sort
-rights_sorted = rights.sort
-
-distance = 0
-(0..lines.size - 1).each do |i|
-  distance += (lefts_sorted[i] - rights_sorted[i]).abs
-end
+distance = lefts.sort.zip(rights.sort).sum { |left, right| (left - right).abs }
 
 puts "part1: #{distance}"
 
-similarity = 0
-
-
-def occurences_of(num, arr)
-  arr.count { |n| n == num }
-end
-
-(0..lefts.size - 1).each do |i|
-  nb_occurences = occurences_of(lefts[i], rights)
-  similarity += nb_occurences * lefts[i]
-end
+similarity = lefts.sum { |left| rights.count(left) * left }
 
 puts "part2: #{similarity}"
