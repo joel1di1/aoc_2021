a395ef36dfcf2917fb1ff96f54928bcdd11ce2a5
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2024-12-01 10:14:34 +0100

    day1 part2

--- 2024/01..09/day1.rb
+++ 2024/01..09/day1.rb
@@ -13,12 +13,26 @@ lines.each do |line|
   rights << right.to_i
 end
 
-lefts.sort!
-rights.sort!
+lefts_sorted = lefts.sort
+rights_sorted = rights.sort
 
 distance = 0
 (0..lines.size - 1).each do |i|
-  distance += (lefts[i] - rights[i]).abs
+  distance += (lefts_sorted[i] - rights_sorted[i]).abs
 end
 
 puts "part1: #{distance}"
+
+similarity = 0
+
+
+def occurences_of(num, arr)
+  arr.count { |n| n == num }
+end
+
+(0..lefts.size - 1).each do |i|
+  nb_occurences = occurences_of(lefts[i], rights)
+  similarity += nb_occurences * lefts[i]
+end
+
+puts "part2: #{similarity}"
