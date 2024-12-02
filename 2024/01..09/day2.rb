1e1a3a641ee40f97920ca6e235dea6d0f3e0cbfa
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2024-12-02 08:01:49 +0100

    improve code

--- 2024/01..09/day2.rb
+++ 2024/01..09/day2.rb
@@ -1,31 +1,15 @@
-require 'readline'
-require 'byebug'
-require 'set'
-require_relative '../../fwk'
-
-lines = File.readlines(File.join(__dir__, 'input2.txt'))
-
-positive = proc { |x| x > 0 }
-negative = proc { |x| x < 0 }
+lines = File.readlines(File.join(__dir__, 'input2.txt')).map(&:strip)
 
 safes = lines.map do |line|
   reports = line.split.map(&:to_i)
   safe = true
-  case reports[0] - reports[1]
-    when 0
-      safe = false
-      inc = 0
-    when positive
-      inc = -1
-    when negative
-      inc = 1
-  end
+  inc = reports[0] <=> reports[1]
 
   (0...reports.size - 1).each do |i|
-    diff = -(reports[i] - reports[i + 1])
-    if !(diff * inc > 0 && diff * inc <= 3)
+    diff = reports[i] - reports[i + 1]
+    if !(diff * inc > 0 && diff.abs <= 3)
       safe = false
-      break 0
+      break
     end
   end
 
