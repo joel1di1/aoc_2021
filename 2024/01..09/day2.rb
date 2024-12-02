10aab1f0f78938bae804fdb6ae2b9f6045d9f682
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2024-12-02 09:08:33 +0100

    improve 2024 day2 part2

--- 2024/01..09/day2.rb
+++ 2024/01..09/day2.rb
@@ -3,33 +3,28 @@ require 'byebug'
 lines = File.readlines(File.join(__dir__, 'input2.txt')).map(&:strip)
 
 def safe_report_rec?(reports)
-  safe = safe_report_inc?(reports)
-  return safe if safe == 1
+  return 1 if safe_report_inc?(reports)
 
-  (0...reports.size).each do |i|
-    safe = safe_report_inc?(reports[0...i] + reports[i + 1..])
-    return safe if safe == 1
+  reports.each_with_index do |_, i|
+    return 1 if safe_report_inc?(reports[0...i] + reports[i + 1..])
   end
 
-  return 0
+  0
 end
 
-def safe_report_inc?(reports, jokers = 1)
-  return 0 if jokers < 0
-
+def safe_report_inc?(reports)
   inc = reports[0] <=> reports[1]
 
-  (0...reports.size - 1).each do |i|
-    diff = reports[i] - reports[i + 1]
-    if !(diff * inc > 0 && diff.abs <= 3)
-      return 0
-    end
+  reports.each_cons(2) do |a, b|
+    diff = a - b
+    return 0 unless diff * inc > 0 && diff.abs <= 3
   end
+
   1
 end
 
-safes1 = lines.map { |line| safe_report_inc?(line.split.map(&:to_i)) }.sum
-safes2 = lines.map { |line| safe_report_rec?(line.split.map(&:to_i)) }.sum
+safes1 = lines.sum { |line| safe_report_inc?(line.split.map(&:to_i)) }
+safes2 = lines.sum { |line| safe_report_rec?(line.split.map(&:to_i)) }
 
 puts "part1: #{safes1}"
 puts "part2: #{safes2}"
