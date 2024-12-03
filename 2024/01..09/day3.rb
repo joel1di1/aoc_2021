4c621025ab1e96be7dec827b77ad018174deaf48
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2024-12-03 08:07:49 +0100

    2024 day3 part2

--- 2024/01..09/day3.rb
+++ 2024/01..09/day3.rb
@@ -12,3 +12,22 @@ sum = lines.map do |line|
 end.sum
 
 puts "part1: #{sum}"
+
+regexp = /mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/
+
+enabled = true
+sum = lines.map do |line|
+  line.scan(regexp).map do |match|
+    if match == "do()"
+      enabled = true
+      0
+    elsif match == "don't()"
+      enabled = false
+      0
+    else
+      enabled ? match.scan(/\d{1,3}/).map(&:to_i).reduce(:*) : 0
+    end
+  end.sum
+end.sum
+
+puts "part2: #{sum}"
