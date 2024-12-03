768e1bf95e11dbd59a0e07fa5ebcf36734187001
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2024-12-03 08:10:20 +0100

    remove debug

--- 2024/01..09/day3.rb
+++ 2024/01..09/day3.rb
@@ -6,12 +6,12 @@ regexp = /mul\(\d{1,3},\d{1,3}\)/
 
 sum = lines.map do |line|
   line.scan(regexp).map do |match|
-    puts match
     match.scan(/\d{1,3}/).map(&:to_i).reduce(:*)
   end.sum
 end.sum
 
 puts "part1: #{sum}"
+
 
 regexp = /mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/
 
