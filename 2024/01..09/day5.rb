0a4ad76543fe3819e513efb4a4d23a672528a4b4
Benoit Lafontaine <benoit.lafontaine@clickandboat.com>
2024-12-14 15:56:06 +0100

    2025 day 5 part2

--- 2024/01..09/day5.rb
+++ 2024/01..09/day5.rb
@@ -22,9 +22,6 @@ part1 = updates.reject do |update|
     current_value = update[i]
     must_be_after = rules[current_value] || []
 
-    puts "update: #{update}, i: #{i}, current_value: #{current_value}, must_be_after: #{must_be_after}"
-    puts "res: #{update[0...i] & must_be_after}\n"
-
     res = (update[0...i] & must_be_after)
     res.size > 0
   end
@@ -34,3 +31,30 @@ end.map do |update|
 end
 
 puts "part1: #{part1.sum}"
+
+incorrectly_ordered = updates.select do |update|
+  (0..update.size-1).any? do |i|
+    current_value = update[i]
+    must_be_after = rules[current_value] || []
+
+    res = (update[0...i] & must_be_after)
+    res.size > 0
+  end
+end
+
+sum = incorrectly_ordered.map do |update|
+  mid_value = nil;
+  update.each_with_index do |value, i|
+    if mid_value.nil?
+      must_be_after = rules[value] || []
+
+      res = update & must_be_after
+      if res.size == (update.size/2)
+        mid_value = value
+      end
+    end
+  end
+  mid_value
+end.sum
+
+puts "part2: #{sum}"
