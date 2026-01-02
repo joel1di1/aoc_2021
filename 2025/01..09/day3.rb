7aa5c0591ec4ea71216ece78ebf22065ec291093
BLA <benoit.lafontaine@clickandboat.com>
2026-01-02 23:58:00 +0100

    day 3 part 1

--- 2025/01..09/day3.rb
+++ 2025/01..09/day3.rb
@@ -4,9 +4,16 @@ lines = File.readlines(File.join(__dir__, 'input3.txt'))
 
 total_output_joltage = lines.map do |line|
   digits = line.chomp.chars.map(&:to_i)
-  dizains = digits[0..-2].max
-  units = digits[(digits.find_index(dizains) + 1)..].max
-  (dizains * 10) + units
+
+  d_size = 2
+  joltage = 0
+  start_index = 0
+
+  (d_size..0).each do |d_index|
+    new_d = digits[start_index..-d_index].max
+    joltage += new_d ** 10
+    start_index = digits.find_index(new_d)
+  end
 end.sum
 
 puts "part1 : #{total_output_joltage}"
