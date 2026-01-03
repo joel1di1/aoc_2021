013add11195150c3f2f9f937e4cfbe169f796bde
BLA <benoit.lafontaine@clickandboat.com>
2026-01-03 01:29:05 +0100

    refacto day 5 with help of codex

--- 2025/01..09/day5.rb
+++ 2025/01..09/day5.rb
@@ -1,53 +1,36 @@
 require_relative '../../fwk'
 
-lines = File.readlines(File.join(__dir__, 'input5.txt'))
+def parse_input(path)
+  ranges_block, ids_block = File.read(path).split("\n\n", 2)
 
-ranges = []
+  ranges = ranges_block.lines.map do |line|
+    left, right = line.split('-').map(&:to_i)
+    Range.new(left, right)
+  end
 
-line_index = 0
-loop do 
-  line = lines[line_index].chomp
-  line_index += 1
-  break if line.empty?
-
-  left, right = line.split('-').map(&:to_i)
-  ranges << (left..right)
+  [ranges, ids_block.lines.map(&:to_i)]
 end
 
-ingredient_ids = lines[line_index..].map(&:to_i)
-
-fresh_count = ingredient_ids.select { |id| ranges.any? { |range| range.include?(id) } }.count
-
-puts "part 1 : #{fresh_count}"
-
-def ranges_overlap?(a, b)
-  a.include?(b.begin) || b.include?(a.begin)
-end
-
-def merge_ranges(a, b)
-  [a.begin, b.begin].min..[a.end, b.end].max
-end
-
-non_overlapping_ranges = []
-
-overlapping_ranges = ranges.clone
-
-until overlapping_ranges.empty?
-  range = overlapping_ranges.first
-  overlapping_ranges.shift
-  index = 0
-  while index < overlapping_ranges.size
-    other_range = overlapping_ranges[index]
-    if ranges_overlap?(range, other_range)
-      range = merge_ranges(range, other_range) 
-      overlapping_ranges.delete(other_range)
-      index = 0
-      next 
+def merge_overlaps(ranges)
+  ranges.sort_by(&:begin).each_with_object([]) do |range, merged|
+    if merged.empty?
+      merged << range
+      next
     end
 
-    index += 1
+    last = merged.last
+    if last.cover?(range.begin)
+      merged[-1] = Range.new(last.begin, [last.end, range.end].max)
+    else
+      merged << range
+    end
   end
-  non_overlapping_ranges << range
 end
 
-puts "part 2 : #{non_overlapping_ranges.map(&:size).sum}"
+ranges, ingredient_ids = parse_input(File.join(__dir__, 'input5.txt'))
+
+merged_ranges = merge_overlaps(ranges)
+fresh_count = ingredient_ids.count { |id| merged_ranges.any? { |range| range.cover?(id) } }
+
+puts "part 1 : #{fresh_count}"
+puts "part 2 : #{merged_ranges.sum(&:size)}"
