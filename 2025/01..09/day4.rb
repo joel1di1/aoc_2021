4437224b560017b0a5b6fb0c628c78df07a00082
BLA <benoit.lafontaine@clickandboat.com>
2026-01-03 01:32:45 +0100

    refacto day 4 with help of codex

--- 2025/01..09/day4.rb
+++ 2025/01..09/day4.rb
@@ -1,53 +1,67 @@
 require_relative '../../fwk'
 
-lines = File.readlines(File.join(__dir__, 'input4.txt'))
+def parse_grid(path)
+  rows = File.readlines(path, chomp: true)
+  height = rows.size
+  width = rows.first.size
 
-MAX_X = lines.first.size
-MAX_Y = lines.size
-
-GRID = {} # rubocop:disable Style/MutableConstant
-lines.each_with_index do |line, x|
-  line.chars.each_with_index do |c, y|
-    GRID[[x, y]] = c
-  end
-end
-
-def neighbors(x, y)
-  [[x-1, y-1], [x, y-1], [x+1, y-1],
-   [x-1, y], [x+1, y],
-   [x-1, y+1], [x, y+1], [x+1, y+1]].select { |a, b| a >= 0 && a < MAX_X && b >= 0 && b < MAX_Y }
-end
-
-def liftable?(x, y)
-  return false if GRID[[x, y]] != '@'
-  
-  neighbors(x, y).select { |neigbor| GRID[neigbor] == '@' }.count < 4
-end
-
-liftable = 0
-(0...MAX_X).each do |x|
-  (0...MAX_Y).each do |y|
-    liftable += 1 if liftable?(x, y)
-  end
-end
-
-puts "part 1 : #{liftable}"
-
-
-initial_roll_count = GRID.values.select { |p| p == '@' }.count
-to_lift = [[-1, -1]]
-
-until to_lift.empty? 
-  to_lift.each { |roll| GRID.delete(roll) }
-  to_lift = []
-  
-  (0...MAX_X).each do |x|
-    (0...MAX_Y).each do |y|
-      to_lift << [x, y] if liftable?(x, y)
+  grid = {}
+  rows.each_with_index do |row, x|
+    row.chars.each_with_index do |char, y|
+      grid[[x, y]] = char
     end
   end
 
+  [grid, width, height]
+end 
+
+def neighbors(x, y, width, height)
+  [
+    [x - 1, y - 1], [x, y - 1], [x + 1, y - 1],
+    [x - 1, y],                 [x + 1, y],
+    [x - 1, y + 1], [x, y + 1], [x + 1, y + 1]
+  ].select { |a, b| a.between?(0, width - 1) && b.between?(0, height - 1) }
 end
 
+def liftable?(grid, width, height, x, y)
+  return false unless grid[[x, y]] == '@'
 
-puts "part 2 : #{initial_roll_count - GRID.values.select { |p| p == '@' }.count}"
+  neighbors(x, y, width, height).count { |neighbor| grid[neighbor] == '@' } < 4
+end
+
+def count_liftable(grid, width, height)
+  (0...width).sum do |x|
+    (0...height).count { |y| liftable?(grid, width, height, x, y) }
+  end
+end
+
+def lift_until_stable(grid, width, height)
+  remaining = grid.dup
+
+  loop do
+    liftable_positions = []
+
+    (0...width).each do |x|
+      (0...height).each do |y|
+        liftable_positions << [x, y] if liftable?(remaining, width, height, x, y)
+      end
+    end
+
+    break if liftable_positions.empty?
+
+    liftable_positions.each { |pos| remaining.delete(pos) }
+  end
+
+  remaining
+end
+
+grid, width, height = parse_grid(File.join(__dir__, 'input4.txt'))
+
+part1 = count_liftable(grid, width, height)
+remaining_grid = lift_until_stable(grid, width, height)
+
+initial_roll_count = grid.values.count('@')
+final_roll_count = remaining_grid.values.count('@')
+
+puts "part 1 : #{part1}"
+puts "part 2 : #{initial_roll_count - final_roll_count}"
