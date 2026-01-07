aaffa27f7bc142a70772c92bd70bfe2c157067b8
BLA <benoit.lafontaine@clickandboat.com>
2026-01-07 01:01:39 +0100

    day 11 part 2

--- 2025/day11.rb
+++ 2025/day11.rb
@@ -2,74 +2,44 @@ require_relative '../fwk'
 require 'matrix'
 require 'glpk'
 
-class Path
-  attr_accessor :devices, :device_set
+class Node
+  attr_accessor :name, :neighbors, :paths_count
 
-  def initialize(devices)
-    self.devices = devices
-    self.device_set = devices.to_set
+  def initialize(name)
+    self.name = name
+    self.neighbors = []
+    self.paths_count = {}
   end
 
-  def append(device)
-    Path.new(devices + [device])
-  end
+  def compute_paths_count(not_visited)
+    return paths_count[not_visited] if paths_count.key?(not_visited)
 
-  def cycle?
-    devices.length != device_set.length
-  end
+    return not_visited.empty? ? 1 : 0 if name == 'out'
 
-  def end?
-    devices.last == 'out'
-  end
+    next_not_visited = not_visited.dup
+    next_not_visited.delete(name)
 
-  def next_paths(devices_map)
-    last_device = devices.last
-    neighbors = devices_map[last_device] || []
-    neighbors.map { |neighbor| append(neighbor) }
+    paths_count[not_visited] = neighbors.map do |neighbor| 
+      neighbor.compute_paths_count(next_not_visited)
+    end.sum
   end
 end
-
 
 lines = File.readlines(File.join(__dir__, 'input11.txt'), chomp: true)
 
 devices_map = {}
+devices_map['out'] = Node.new('out')
 lines.each do |line|
-  device, neighbors_str = line.split(':')
-  neighbors = neighbors_str.split.map(&:strip)
-  devices_map[device] = neighbors
+  name, _neighbors_str = line.split(':')
+  devices_map[name] = Node.new(name)
 end
 
-paths = [Path.new(['you'])]
-ended_paths = []
-until paths.empty?
-  path = paths.pop
-
-  next if path.cycle?
-
-  if path.end?
-    ended_paths << path
-    next
-  end
-
-  paths += path.next_paths(devices_map)
+lines.each do |line| # rubocop:disable Style/CombinableLoops
+  device_name, neighbors_str = line.split(':')
+  neighbors = neighbors_str.split.map(&:strip).map { |name| devices_map[name] }
+  device = devices_map[device_name]
+  device.neighbors = neighbors
 end
 
-puts "part 1 : #{ended_paths.count}"
-
-# paths = [Path.new(['svr'])]
-# ended_paths = []
-# until paths.empty?
-#   path = paths.pop
-
-#   next if path.cycle?
-
-#   if path.end?
-#     ended_paths << path
-#     next
-#   end
-
-#   paths += path.next_paths(devices_map)
-# end
-
-puts "part 2 : #{ended_paths.count}"
-
+puts "part 1 : #{devices_map['you'].compute_paths_count([])}"
+puts "part 2 : #{devices_map['svr'].compute_paths_count(['dac', 'fft'])}"
--- 2025/day11_codex.rb
+++ 2025/day11_codex.rb
@@ -12,48 +12,48 @@ lines.each do |line|
   graph[name] = outputs
 end
 
+def count_basic_recursive(node, graph, memo)
+  return 1 if node == "out"
+  return memo[node] if memo.key?(node)
+
+  total = graph[node].sum { |child| count_basic_recursive(child, graph, memo) }
+  memo[node] = total
+end
+
 def count_paths_basic(graph, start_node)
   memo = {}
-
-  count = lambda do |node|
-    return 1 if node == "out"
-    return memo[node] if memo.key?(node)
-
-    total = graph[node].sum { |child| count.call(child) }
-    memo[node] = total
-  end
-
   return 0 unless graph.key?(start_node)
 
-  count.call(start_node)
+  count_basic_recursive(start_node, graph, memo)
+end
+
+def count_with_devices_recursive(node, visited, graph, must_visit, memo)
+  return visited.all? ? 1 : 0 if node == "out"
+
+  key = [node, visited]
+  return memo[key] if memo.key?(key)
+
+  total = 0
+  graph[node].each do |child|
+    next_visited = visited.dup
+    must_visit.each_with_index do |device, index|
+      next_visited[index] ||= (child == device)
+    end
+    total += count_with_devices_recursive(child, next_visited, graph, must_visit, memo)
+  end
+
+  memo[key] = total
 end
 
 def count_paths_with_devices(graph, start_node, must_visit)
   memo = {}
-
-  count = lambda do |node, visited|
-    return visited.all? ? 1 : 0 if node == "out"
-
-    key = [node, visited]
-    return memo[key] if memo.key?(key)
-
-    total = 0
-    graph[node].each do |child|
-      next_visited = visited.dup
-      must_visit.each_with_index do |device, index|
-        next_visited[index] ||= (child == device)
-      end
-      total += count.call(child, next_visited)
-    end
-
-    memo[key] = total
-  end
-
   return 0 unless graph.key?(start_node)
 
   initial_visited = must_visit.map { |device| start_node == device }
-  count.call(start_node, initial_visited)
+  count_with_devices_recursive(start_node, initial_visited, graph, must_visit, memo)
 end
 
 puts "part 1 : #{count_paths_basic(graph, 'you')}"
 puts "part 2 : #{count_paths_with_devices(graph, 'svr', ['dac', 'fft'])}"
+
+
